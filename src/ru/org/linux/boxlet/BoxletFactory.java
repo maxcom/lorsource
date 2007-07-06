60eedb181d9af3698c48ef8c55cbe7e2dd53b4b1
maxcom <maxcom@3c041eb6-2c19-43cc-91a8-1cbaffc2ceed>
2007-07-06 08:24:22 +0000

    minor cleanup
    git-svn-id: https://svn.linux.org.ru/repos/lor/trunk@203 3c041eb6-2c19-43cc-91a8-1cbaffc2ceed

--- src/ru/org/linux/boxlet/BoxletFactory.java
+++ src/ru/org/linux/boxlet/BoxletFactory.java
@@ -6,18 +6,18 @@ import java.util.Properties;
 import ru.org.linux.util.ProfileHashtable;
 import ru.org.linux.util.UtilException;
 
-public abstract class BoxletFactory
-{
-        final Object config;
+public abstract class BoxletFactory {
+  final Object config;
 
-        public BoxletFactory(Object Config)
-        {
-                config=Config;
-        }
+  public BoxletFactory(Object Config) {
+    config = Config;
+  }
 
-        public abstract String getContent(String name, ProfileHashtable profile) throws IOException, BoxletException, UtilException;
-        public abstract String getMenuContent(String name, ProfileHashtable profile, String addUrl, String removeUrl) throws IOException, BoxletException, UtilException;
+  public abstract String getContent(String name, ProfileHashtable profile) throws IOException, BoxletException, UtilException;
 
-        abstract String getVariantID(String name, ProfileHashtable profile, Properties request) throws BoxletException, UtilException;
-        abstract long getVersionID(String name, ProfileHashtable profile, Properties request) throws BoxletException;
+  public abstract String getMenuContent(String name, ProfileHashtable profile, String addUrl, String removeUrl) throws IOException, BoxletException, UtilException;
+
+  abstract String getVariantID(String name, ProfileHashtable profile, Properties request) throws BoxletException, UtilException;
+
+  abstract long getVersionID(String name, ProfileHashtable profile, Properties request) throws BoxletException;
 }
