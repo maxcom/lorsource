334dad68681c975d88efa907d15a313c97276006
maxcom <maxcom@3c041eb6-2c19-43cc-91a8-1cbaffc2ceed>
2008-07-15 14:30:54 +0000

    Удаление тем и постов с определенного IP - MVC
    git-svn-id: https://svn.linux.org.ru/repos/lor/trunk@1547 3c041eb6-2c19-43cc-91a8-1cbaffc2ceed

---
+++ src/ru/org/linux/spring/DelIPController.java
@@ -0,0 +1,135 @@
+package ru.org.linux.spring;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import ru.org.linux.site.*;
+import ru.org.linux.util.ServletParameterParser;
+
+public class DelIPController extends AbstractController {
+  protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+  
+    Map<String, Object> params = new HashMap<String, Object>();
+    Map<Integer, String> deleted = new HashMap<Integer, String>();
+    
+    Template tmpl = Template.getTemplate(request);
+    HttpSession session = request.getSession();
+    
+    if (!tmpl.isModeratorSession()) {
+      throw new IllegalAccessException("Not authorized");
+    }
+
+    if (!request.getMethod().equals("POST")) {
+      throw new IllegalAccessException("Invalid method");
+    }
+    
+    Connection db = null;
+    try {
+      db = LorDataSource.getConnection();
+
+      String reason = new ServletParameterParser(request).getString("reason");
+      String ip = new ServletParameterParser(request).getString("ip");
+      String time = new ServletParameterParser(request).getString("time");
+
+      Calendar calendar = Calendar.getInstance();
+      calendar.setTime(new Date());
+
+      if ("hour".equals(time)) {
+        calendar.add(Calendar.HOUR_OF_DAY, -1);
+      } else if ("day".equals(time)) {
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+      } else if ("3day".equals(time)) {
+        calendar.add(Calendar.DAY_OF_MONTH, -3);
+      } else {
+        throw new UserErrorException("Invalid count");
+      }
+
+      Timestamp ts = new Timestamp(calendar.getTimeInMillis());
+      params.put("message", "Удаляем темы и сообщения после "+ts.toString()+" с IP "+ip+"<br>");
+
+      db.setAutoCommit(false);
+    
+      User moderator = User.getUser(db, (String) session.getValue("nick"));
+    
+      PreparedStatement st = null;
+      ResultSet rs = null;
+      CommentDeleter deleter = null;
+      
+      try {
+        // Delete IP topics
+        PreparedStatement lock = db.prepareStatement("SELECT id FROM topics WHERE postip=?::inet AND not deleted AND postdate>? FOR UPDATE");
+        PreparedStatement st1 = db.prepareStatement("UPDATE topics SET deleted='t',sticky='f' WHERE id=?");
+        PreparedStatement st2 = db.prepareStatement("INSERT INTO del_info (msgid, delby, reason) values(?,?,?)");
+        lock.setString(1, ip);
+        lock.setTimestamp(2, ts);
+        st2.setInt(2, moderator.getId());
+        st2.setString(3, reason);
+       
+        int topicCounter = 0;
+        ResultSet lockResult = lock.executeQuery(); // lock another delete on this row
+        while (lockResult.next()) {
+          int mid = lockResult.getInt("id");
+          st1.setInt(1,mid);
+          st2.setInt(1,mid);
+          st1.executeUpdate();
+          st2.executeUpdate();
+          topicCounter++;
+        }
+        st1.close();
+        st2.close();
+        lockResult.close();
+        lock.close(); 
+        params.put("topics", topicCounter);
+    
+        // Delete user comments
+        deleter = new CommentDeleter(db);
+    
+        st = db.prepareStatement("SELECT id FROM comments WHERE postip=?::inet AND not deleted AND postdate>? ORDER BY id DESC FOR update");
+        st.setString(1,ip);
+        st.setTimestamp(2, ts);
+
+        rs = st.executeQuery();
+    
+        while(rs.next()) {
+          int msgid = rs.getInt("id");
+          deleted.put(msgid,deleter.deleteReplys(msgid, moderator, true) + 
+            " " + deleter.deleteComment(msgid, reason, moderator, -20));
+        }
+        params.put("deleted", deleted);        
+        
+      } finally {
+        if (deleter!=null) {
+          deleter.close();
+        }
+      
+        if (rs!=null) {
+          rs.close();
+        }
+      
+        if (st!=null) {
+          st.close();
+        }
+      }
+      
+      db.commit();
+      
+      return new ModelAndView("delip", params);
+    } finally {
+      if (db!=null) {
+        db.close();
+      }
+    }
+  }
+}
---
+++ web/WEB-INF/jsp/delip.jsp
@@ -0,0 +1,38 @@
+<%@ page pageEncoding="koi8-r" contentType="text/html; charset=utf-8"%>
+<%@ page import="java.util.Date"   %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<jsp:include page="/WEB-INF/jsp/head.jsp"/>
+<%
+
+  response.setDateHeader("Expires", new Date(new Date().getTime() - 20 * 3600 * 1000).getTime());
+  response.setDateHeader("Last-Modified", new Date(new Date().getTime() - 2 * 1000).getTime());
+
+%>
+<title>delip</title>
+<link rel="parent" title="Linux.org.ru" href="/">
+<jsp:include page="/WEB-INF/jsp/header.jsp"/>
+
+<h1>Удаление тем и сообщений</h1>
+
+${message}
+
+<br/>
+
+Удалено тем: ${topics}
+
+<ul>
+
+  <c:forEach var="del" items="${deleted}">
+
+    <li>
+    
+Удаляется #${del.key}: ${del.value}
+
+    </li>
+                      
+  </c:forEach>
+                        
+</ul>
+
+<jsp:include page="/WEB-INF/jsp/footer.jsp"/>
+
