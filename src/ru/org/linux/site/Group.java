0bcd6538e7a656ef9b9ad5779149e7624885589d
maxcom <maxcom@3c041eb6-2c19-43cc-91a8-1cbaffc2ceed>
2008-07-21 08:56:25 +0000

    isCommentPostingAllowed fixup
    git-svn-id: https://svn.linux.org.ru/repos/lor/trunk@1568 3c041eb6-2c19-43cc-91a8-1cbaffc2ceed

--- src/ru/org/linux/site/Group.java
+++ src/ru/org/linux/site/Group.java
@@ -199,10 +199,6 @@ public class Group {
       return false;
     }
 
-    if (currentUser.getMaxScore()>=100) {
-      return true;
-    }
-
     return currentUser.getScore() >= restrictComments;
   }
 
