8f65fbf0a06f181f7e542025e80d3675bdf39728
Svyatoslav Reyentenko <rsvato@gmail.com>
2009-04-29 18:56:54 +0400

    Introducing Velocity for boxlet displaying

---
+++ src/ru/org/linux/site/boxes/GalleryItem.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 1998-2009 Linux.org.ru
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package ru.org.linux.site.boxes;
+
+import ru.org.linux.util.ImageInfo;
+
+/**
+ * User: rsvato
+* Date: Apr 29, 2009
+* Time: 6:44:26 PM
+*/
+public class GalleryItem {
+  Integer msgid;
+  String nick;
+  String icon;
+  ImageInfo info;
+  ImageInfo imginfo;
+  String title;
+  Integer stat;
+  String url;
+
+  public Integer getMsgid() {
+    return msgid;
+  }
+
+  public void setMsgid(Integer msgid) {
+    this.msgid = msgid;
+  }
+
+  public String getNick() {
+    return nick;
+  }
+
+  public void setNick(String nick) {
+    this.nick = nick;
+  }
+
+  public String getIcon() {
+    return icon;
+  }
+
+  public void setIcon(String icon) {
+    this.icon = icon;
+  }
+
+  public ImageInfo getInfo() {
+    return info;
+  }
+
+  public void setInfo(ImageInfo info) {
+    this.info = info;
+  }
+
+  public ImageInfo getImginfo() {
+    return imginfo;
+  }
+
+  public void setImginfo(ImageInfo imginfo) {
+    this.imginfo = imginfo;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public Integer getStat() {
+    return stat;
+  }
+
+  public void setStat(Integer stat) {
+    this.stat = stat;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+}
--- src/ru/org/linux/site/boxes/gallery.java
+++ src/ru/org/linux/site/boxes/gallery.java
@@ -16,12 +16,19 @@
 package ru.org.linux.site.boxes;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
+import java.util.Vector;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.VelocityContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import ru.org.linux.boxlet.Boxlet;
 import ru.org.linux.site.LorDataSource;
@@ -32,7 +39,66 @@ import ru.org.linux.util.ProfileHashtable;
 import ru.org.linux.util.StringUtil;
 
 public final class gallery extends Boxlet {
-  @Override
+
+  private static final Log log = LogFactory.getLog(gallery.class);
+
+  public String getContent(ProfileHashtable profileHashtable) throws IOException, SQLException {
+    Connection db = null;
+    StringWriter writer = new StringWriter();
+    try {
+      db = LorDataSource.getConnection();
+
+      Statement st = db.createStatement();
+      ResultSet rs = st.executeQuery("SELECT topics.id as msgid, topics.stat1, topics.title, topics.url," +
+        " topics.linktext, nick FROM topics, sections, groups, users WHERE groups.id=topics.groupid" +
+        " AND groups.section=sections.id AND users.id=topics.userid AND topics.moderate AND sections.id=3 AND NOT deleted " +
+        " ORDER BY commitdate DESC LIMIT 3");
+
+      Vector<GalleryItem> items = new Vector<GalleryItem>();
+      while (rs.next()) {
+        GalleryItem item = createGalleryItem(rs);
+        items.add(item);
+      }
+
+      try {
+        VelocityEngine ve = new VelocityEngine();
+        ve.init();
+        VelocityContext vc = new VelocityContext();
+        vc.put("items", items);
+        //todo: template loader
+        ve.mergeTemplate("gallery.vm", "UTF-8", vc, writer);
+      } catch (Exception e) {
+        log.error(e);
+      }
+    } finally {
+      if (db != null) {
+        db.close();
+      }
+    }
+    return writer.toString();
+  }
+
+  private GalleryItem createGalleryItem(ResultSet rs) throws SQLException, IOException {
+    GalleryItem item = new GalleryItem();
+    item.setMsgid(rs.getInt("msgid"));
+    item.setStat(rs.getInt("stat1"));
+    item.setTitle(rs.getString("title"));
+    item.setUrl("url");
+    item.setIcon("linktext");
+    item.setNick("nick");
+
+    String htmlPath = ((PropertiesConfig) config).getProperties().getProperty("HTMLPathPrefix");
+
+    try {
+      item.setInfo(new ImageInfo(htmlPath + item.getIcon()));
+      item.setImginfo(new ImageInfo(htmlPath + item.getUrl()));
+    } catch (BadImageException e) {
+      log.error(e);
+    }
+
+    return item;
+  }
+
   public String getContentImpl(ProfileHashtable profile) throws IOException, SQLException {
     Connection db = null;
     try {
@@ -80,7 +146,7 @@ public final class gallery extends Boxlet {
 
         out.append("<p>");
       }
-      rs.close();                          
+      rs.close();
       out.append("<a href=\"view-news.jsp?section=3\">другие скриншоты...</a>");
       out.append("</div>");
       return out.toString();
