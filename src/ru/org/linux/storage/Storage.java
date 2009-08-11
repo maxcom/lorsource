390351efc0982de8ac18b2e1b8a58a15e6e50ce8
maxcom <maxcom@jet.msk.su>
2009-08-11 12:42:39 +0400

    minor cleanup

--- src/ru/org/linux/storage/Storage.java
+++ src/ru/org/linux/storage/Storage.java
@@ -23,11 +23,6 @@ import gnu.regexp.RE;
 import gnu.regexp.REException;
 
 public abstract class Storage {
-  public String readMessageNull(String domain, String msgid) throws IOException, StorageException {
-    check(domain, msgid);
-    return readMessageNullImpl(domain, msgid);
-  }
-
   public String readMessageDefault(String domain, String msgid, String def) throws IOException, StorageException {
     check(domain, msgid);
     return readMessageDefaultImpl(domain, msgid, def);
@@ -55,44 +50,32 @@ public abstract class Storage {
   }
 
 // private
-  private static RE DomainCheckRE = null;
-  private static RE NameCheckRE = null;
+  private final static RE DOMAIN_CHECK_RE;
+  private final static RE NAME_CHECK_RE;
 
-  private void check(String domain, String msgid) throws StorageException {
+  static {
     try {
-      if (DomainCheckRE == null) {
-        DomainCheckRE = new RE("[a-z]*");
-      }
-
-      if (!DomainCheckRE.isMatch(domain)) {
-        throw new StorageBadDomainException(domain);
-      }
-
-      if (NameCheckRE == null) {
-        NameCheckRE = new RE("([_a-z][a-z0-9_-]*)|([0-9]*)", RE.REG_ICASE);
-      }
-
-      if (!NameCheckRE.isMatch(msgid)) {
-        throw new StorageBadMsgidException(msgid);
-      }
-
+      DOMAIN_CHECK_RE = new RE("[a-z]*");
+      NAME_CHECK_RE = new RE("([_a-z][a-z0-9_-]*)|([0-9]*)", RE.REG_ICASE);
     } catch (REException e) {
-      throw new StorageInternalErrorException(e.toString());
+      throw new RuntimeException(e);
     }
   }
 
-// Implementaion classes
+  private void check(String domain, String msgid) throws StorageException {
+    if (!DOMAIN_CHECK_RE.isMatch(domain)) {
+      throw new StorageBadDomainException(domain);
+    }
+
+    if (!NAME_CHECK_RE.isMatch(msgid)) {
+      throw new StorageBadMsgidException(msgid);
+    }
+  }
+
+// Implementaion methods
 
   protected String readMessageImpl(String domain, String msgid) throws IOException, StorageException {
     throw new StorageNotImplException();
-  }
-
-  protected String readMessageNullImpl(String domain, String msgid) throws IOException, StorageException {
-    try {
-      return readMessageImpl(domain, msgid);
-    } catch (StorageNotFoundException e) {
-      return null;
-    }
   }
 
   protected String readMessageDefaultImpl(String domain, String msgid, String def) throws IOException, StorageException {
