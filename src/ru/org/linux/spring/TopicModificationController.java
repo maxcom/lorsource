bdfb2e4f688ada7a9f9cb7be152ea437f076290d
maxcom <maxcom@jet.msk.su>
2010-01-15 14:12:57 +0300

    mt.jsp: перевод на mvc; cleanup

--- src/ru/org/linux/spring/TopicModificationController.java
+++ src/ru/org/linux/spring/TopicModificationController.java
@@ -17,6 +17,7 @@ package ru.org.linux.spring;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 import javax.servlet.ServletRequest;
 import org.springframework.context.support.ApplicationObjectSupport;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import ru.org.linux.site.*;
 
@@ -158,6 +160,109 @@ public class TopicModificationController extends ApplicationObjectSupport {
       mv.getModel().put("message", message);
 
       mv.getModel().put("groups", Group.getGroups(db, message.getSection()));
+
+      return mv;
+    } finally {
+      if (db!=null) {
+        db.close();
+      }
+    }
+  }
+
+  @RequestMapping(value="/mt.jsp", method=RequestMethod.POST)
+  public ModelAndView moveTopic(
+    ServletRequest request,
+    @RequestParam int msgid,
+    @RequestParam("moveto") int newgr
+  ) throws Exception {
+    Template tmpl = Template.getTemplate(request);
+
+    if (!tmpl.isModeratorSession()) {
+      throw new AccessViolationException("Not moderator");
+    }
+
+    Connection db = null;
+
+    try {
+      db = LorDataSource.getConnection();
+      db.setAutoCommit(false);
+
+      Message msg = new Message(db, msgid);
+
+      Statement st1 = db.createStatement();
+
+      Group newGrp = new Group(db, newgr);
+      String url = msg.getUrl();
+
+      PreparedStatement movePst = db.prepareStatement("UPDATE topics SET groupid=?,lastmod=CURRENT_TIMESTAMP WHERE id=?");
+      movePst.setInt(1, newGrp.getId());
+      movePst.setInt(2, msgid);
+
+      movePst.executeUpdate();
+
+      if (url != null && !newGrp.isLinksAllowed() && !newGrp.isImagePostAllowed()) {
+        String sSql = "UPDATE topics SET linktext=null, url=null WHERE id=" + msgid;
+
+        String title = msg.getGroupTitle();
+        String linktext = msg.getLinktext();
+
+        st1.executeUpdate(sSql);
+
+        /* if url is not null, update the topic text */
+        PreparedStatement pst1 = db.prepareStatement("UPDATE msgbase SET message=message||? WHERE id=?");
+
+        String link;
+        if (msg.isLorcode()) {
+          link = "\n[url=" + url + ']' + linktext + "[/url]\n";
+        } else {
+          link = "<br><a href=\"" + url + "\">" + linktext + "</a>\n<br>\n";
+        }
+
+        if (msg.isLorcode()) {
+          pst1.setString(1, '\n' + link + "\n[i]Перемещено " + tmpl.getNick() + " из " + title + "[/i]\n");
+        } else {
+          pst1.setString(1, '\n' + link + "<br><i>Перемещено " + tmpl.getNick() + " из " + title + "</i>\n");
+        }
+
+        pst1.setInt(2, msgid);
+        pst1.executeUpdate();
+      }
+
+      logger.info("topic " + msgid + " moved" +
+              " by " + tmpl.getNick() + " from news/forum " + msg.getGroupTitle() + " to forum " + newGrp.getTitle());
+      db.commit();
+
+      return new ModelAndView(new RedirectView(msg.getLinkLastmod()));
+    } finally {
+      if (db!=null) {
+        db.close();
+      }
+    }
+  }
+
+  @RequestMapping(value="/mt.jsp", method=RequestMethod.GET)
+  public ModelAndView moveTopicFormForum(
+    ServletRequest request,
+    @RequestParam int msgid
+  ) throws Exception {
+    Template tmpl = Template.getTemplate(request);
+
+    if (!tmpl.isModeratorSession()) {
+      throw new IllegalAccessException("Not authorized");
+    }
+
+    ModelAndView mv = new ModelAndView("mtn");
+
+    Connection db = null;
+
+    try {
+      db = LorDataSource.getConnection();
+
+      Message message = new Message(db, msgid);
+
+      mv.getModel().put("message", message);
+
+      mv.getModel().put("groups", Group.getGroups(db, new Section(db, 2)));
 
       return mv;
     } finally {
