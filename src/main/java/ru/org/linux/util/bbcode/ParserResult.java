3fc3a6486d53a3b1d803a1381f3b9980c4db7d70
Ildar M. Hizbulin <hizel@vyborg.ru>
2011-08-24 19:50:15 +0400

    вариант bb2html который вернет можество не забаненых пользователей упомянутых в сообщении

---
+++ src/main/java/ru/org/linux/util/bbcode/ParserResult.java
@@ -0,0 +1,11 @@
+package ru.org.linux.util.bbcode;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hizel
+ * Date: 8/24/11
+ * Time: 7:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParserResult {
+}
