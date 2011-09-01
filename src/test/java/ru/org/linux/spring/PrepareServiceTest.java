b3cfb383ee78be440da1689dd858329d926a582d
Ildar Hizbulin <hizel@vyborg.ru>
2011-09-01 12:48:45 +0400

    подготовка к тестированию Model

---
+++ src/test/java/ru/org/linux/spring/PrepareServiceTest.java
@@ -0,0 +1,11 @@
+package ru.org.linux.spring;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hizel
+ * Date: 9/1/11
+ * Time: 12:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PrepareServiceTest {
+}
