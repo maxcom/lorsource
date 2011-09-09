8d3794eff1ec4f9b64f95e9acbc75e5f03bf6458
Ildar Hizbulin <hizel@vyborg.ru>
2011-09-09 14:55:41 +0400

    убран случайный ненужный import

--- src/main/java/ru/org/linux/spring/ReplyFeedView.java
+++ src/main/java/ru/org/linux/spring/ReplyFeedView.java
@@ -21,7 +21,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import com.sun.org.apache.regexp.internal.RE;
 import com.sun.syndication.feed.synd.*;
 
 public class ReplyFeedView extends AbstractRomeView {
