b747e5f4e24d099605ea93366b4c86662f5eeedf
Maxim Valyanskiy <max.valjanski@gmail.com>
2011-09-13 21:33:32 +0400

    забыл файл

---
+++ src/main/java/ru/org/linux/spring/validators/AddMessageRequestValidator.java
@@ -0,0 +1,44 @@
+package ru.org.linux.spring.validators;
+
+import org.jdom.Verifier;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.org.linux.spring.AddMessageForm;
+import ru.org.linux.spring.AddMessageRequest;
+
+public class AddMessageRequestValidator implements Validator {
+  @Override
+  public boolean supports(Class<?> clazz) {
+    return AddMessageRequest.class.equals(clazz);
+  }
+
+  @Override
+  public void validate(Object target, Errors errors) {
+    AddMessageRequest form = (AddMessageRequest) target;
+
+    if (form.getGroup()==null) {
+      errors.rejectValue("group", null, "Группа не задана");
+    }
+
+    String title = form.getTitle();
+
+    if (title!=null && "".equals(title.trim())) {
+      errors.rejectValue("title", null, "заголовок сообщения не может быть пустым");
+    }
+
+    if (title!=null && title.length() > AddMessageForm.MAX_TITLE_LENGTH) {
+      errors.rejectValue("title", null, "Слишком большой заголовок");
+    }
+
+    if (form.getMsg() != null) {
+      String error = Verifier.checkCharacterData(form.getMsg());
+      if (error != null) {
+        errors.rejectValue("msg", null, error);
+      }
+    }
+
+    if (form.getUrl()!=null && form.getUrl().length() > AddMessageForm.MAX_URL_LENGTH) {
+      errors.rejectValue("url", null, "Слишком длинный URL");
+    }
+  }
+}
