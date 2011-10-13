64ed3d091db73d8bca881e03550e3d8352c2cb63
Maxim Valyanskiy <max.valjanski@gmail.com>
2011-10-13 17:52:19 +0400

    merge SectionRSSController with NewsViewerController

--- src/main/java/ru/org/linux/spring/NewsViewerController.java
+++ src/main/java/ru/org/linux/spring/NewsViewerController.java
@@ -37,19 +37,20 @@ import ru.org.linux.util.DateUtil;
 import ru.org.linux.util.ServletParameterException;
 import ru.org.linux.util.ServletParameterMissingException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 public class NewsViewerController {
+  private static final String[] filterValues = { "all", "notalks", "tech"};
+  private static final Set<String> filterValuesSet = new HashSet<String>(Arrays.asList(filterValues));
+
   @Autowired
   private SectionDao sectionDao;
 
@@ -660,5 +661,91 @@ public class NewsViewerController {
     mv.getModel().put("params", null);
 
     return mv;
+  }
+
+  @RequestMapping("/section-rss.jsp")
+  public ModelAndView showRSS(
+    @RequestParam(value="filter", required = false) String filter,
+    @RequestParam(value="section", required = false) Integer sectionId,
+    @RequestParam(value="group", required = false) Integer groupId,
+    HttpServletRequest request
+  ) throws Exception {
+
+    if (filter!=null && !filterValuesSet.contains(filter)) {
+      throw new UserErrorException("Некорректное значение filter");
+    }
+
+    Map<String, Object> params = new HashMap<String, Object>();
+
+    if (sectionId==null) {
+      sectionId = 1;
+    }
+
+    if (groupId==null) {
+      groupId = 0;
+    }
+
+    boolean notalks = filter!=null && "notalks".equals(filter);
+    boolean tech = filter!=null && "tech".equals(filter);
+
+    String userAgent = request.getHeader("User-Agent");
+    final boolean feedBurner = userAgent!=null && userAgent.contains("FeedBurner");
+
+    if (sectionId==1 && groupId==0 && !notalks && !tech && !feedBurner && request.getParameter("noredirect")==null) {
+      return new ModelAndView(new RedirectView("http://feeds.feedburner.com/org/LOR"));
+    }
+
+    final NewsViewer nv = new NewsViewer();
+    nv.addSection(sectionId);
+    nv.setDatelimit(" postdate>(CURRENT_TIMESTAMP-'3 month'::interval) ");
+
+    if (groupId !=0) {
+      nv.setGroup(groupId);
+    }
+
+    nv.setNotalks(notalks);
+    nv.setTech(tech);
+
+    nv.setMainUrl(Template.getTemplate(request).getMainUrl());
+
+    nv.setLimit("LIMIT 20");
+
+    Section section = sectionDao.getSection(sectionId);
+    params.put("section", section);
+
+    if (section.isPremoderated()) {
+      nv.setCommitMode(NewsViewer.CommitMode.COMMITED_ONLY);
+    } else {
+      nv.setCommitMode(NewsViewer.CommitMode.POSTMODERATED_ONLY);
+    }
+
+    Group group = null;
+    if (groupId != 0) {
+      group = groupDao.getGroup(groupId);
+
+      if (group.getSectionId() != sectionId) {
+        throw new BadGroupException("группа #" + groupId + " не принадлежит разделу #" + sectionId);
+      }
+
+      params.put("group", group);
+    }
+
+    String ptitle = section.getName();
+    if (group != null) {
+      ptitle += " - " + group.getTitle();
+    }
+
+    params.put("ptitle", ptitle);
+
+    List<Message> messages = jdbcTemplate.execute(new ConnectionCallback<List<Message>>() {
+      @Override
+      public List<Message> doInConnection(Connection con) throws SQLException, DataAccessException {
+        return feedBurner ? nv.getMessages(con) : nv.getMessagesCached(con);
+      }
+    });
+
+    params.put("messages", prepareService.prepareMessages(messages, true));
+
+    return new ModelAndView("section-rss", params);
   }
 }
