fe8476853819ee1d2815738c4ec8709e51bddf14
Maxim Valyanskiy <max.valjanski@gmail.com>
2011-10-13 17:50:27 +0400

    SectionRSSController: use Dao

--- src/main/java/ru/org/linux/spring/SectionRSSController.java
+++ src/main/java/ru/org/linux/spring/SectionRSSController.java
@@ -16,16 +16,22 @@
 package ru.org.linux.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import ru.org.linux.site.*;
+import ru.org.linux.spring.dao.GroupDao;
 import ru.org.linux.spring.dao.SectionDao;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.*;
 
 @Controller
@@ -35,6 +41,19 @@ public class SectionRSSController {
 
   @Autowired
   private SectionDao sectionDao;
+
+  @Autowired
+  private GroupDao groupDao;
+
+  @Autowired
+  private PrepareService prepareService;
+
+  private JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  public void setDataSource(DataSource ds) {
+    jdbcTemplate = new JdbcTemplate(ds);
+  }
 
   @RequestMapping("/section-rss.jsp")
   public ModelAndView showRSS(
@@ -48,7 +67,7 @@ public class SectionRSSController {
       throw new UserErrorException("Некорректное значение filter");
     }
     
-    Map params = new HashMap();
+    Map<String, Object> params = new HashMap<String, Object>();
 
     if (sectionId==null) {
       sectionId = 1;
@@ -62,13 +81,13 @@ public class SectionRSSController {
     boolean tech = filter!=null && "tech".equals(filter);
     
     String userAgent = request.getHeader("User-Agent");
-    boolean feedBurner = userAgent!=null && userAgent.contains("FeedBurner");
+    final boolean feedBurner = userAgent!=null && userAgent.contains("FeedBurner");
 
     if (sectionId==1 && groupId==0 && !notalks && !tech && !feedBurner && request.getParameter("noredirect")==null) {
       return new ModelAndView(new RedirectView("http://feeds.feedburner.com/org/LOR"));
     }
 
-    NewsViewer nv = new NewsViewer();
+    final NewsViewer nv = new NewsViewer();
     nv.addSection(sectionId);
     nv.setDatelimit(" postdate>(CURRENT_TIMESTAMP-'3 month'::interval) ");
 
@@ -86,42 +105,39 @@ public class SectionRSSController {
     Section section = sectionDao.getSection(sectionId);
     params.put("section", section);
 
-    Connection db = null;
-    try {
-      db = LorDataSource.getConnection();
-
-      if (section.isPremoderated()) {
-        nv.setCommitMode(NewsViewer.CommitMode.COMMITED_ONLY);
-      } else {
-        nv.setCommitMode(NewsViewer.CommitMode.POSTMODERATED_ONLY);
-      }
-
-      Group group = null;
-      if (groupId!=0) {
-        group = Group.getGroup(db, groupId);
-
-        if (group.getSectionId()!=sectionId) {
-          throw new BadGroupException("группа #"+groupId+" не принадлежит разделу #"+sectionId);
-        }
-
-        params.put("group", group);
-      }
-
-      String ptitle = section.getName();
-      if (group!=null) {
-        ptitle += " - " + group.getTitle();
-      }
-
-      params.put("ptitle", ptitle);
-
-      List<PreparedMessage> messages=feedBurner?nv.getPreparedMessages(db):nv.getPreparedMessagesCached(db);
-      params.put("messages", messages);
-
-      return new ModelAndView("section-rss", params);
-    } finally {
-      if (db!=null) {
-        db.close();
-      }
+    if (section.isPremoderated()) {
+      nv.setCommitMode(NewsViewer.CommitMode.COMMITED_ONLY);
+    } else {
+      nv.setCommitMode(NewsViewer.CommitMode.POSTMODERATED_ONLY);
     }
+
+    Group group = null;
+    if (groupId != 0) {
+      group = groupDao.getGroup(groupId);
+
+      if (group.getSectionId() != sectionId) {
+        throw new BadGroupException("группа #" + groupId + " не принадлежит разделу #" + sectionId);
+      }
+
+      params.put("group", group);
+    }
+
+    String ptitle = section.getName();
+    if (group != null) {
+      ptitle += " - " + group.getTitle();
+    }
+
+    params.put("ptitle", ptitle);
+
+    List<Message> messages = jdbcTemplate.execute(new ConnectionCallback<List<Message>>() {
+      @Override
+      public List<Message> doInConnection(Connection con) throws SQLException, DataAccessException {
+        return feedBurner ? nv.getMessages(con) : nv.getMessagesCached(con);
+      }
+    });
+
+    params.put("messages", prepareService.prepareMessages(messages, true));
+
+    return new ModelAndView("section-rss", params);
   }
 }
