a2b6282a8725ba6c9883169afaeefe61aae250d7
Ildar Hizbulin <hizel@vyborg.ru>
2011-10-26 14:10:05 +0400

    окончательная замена HTMLFormatter на несколько отдельных классов в рамках #60 осталось тесты переписать

---
+++ src/main/java/ru/org/linux/util/formatter/ToHtmlFormatter.java
@@ -0,0 +1,11 @@
+package ru.org.linux.util.formatter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hizel
+ * Date: 10/26/11
+ * Time: 11:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ToHtmlFormatter {
+}
