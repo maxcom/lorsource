385f00c636fcfbf5a3cbf44040551f0dd00e03d9
Ildar Hizbulin <hizel@vyborg.ru>
2011-11-02 13:33:31 +0400

    голосование отображается простой полоской вместо пингвинчиков

--- src/main/java/ru/org/linux/util/StringUtil.java
+++ src/main/java/ru/org/linux/util/StringUtil.java
@@ -165,4 +165,20 @@ public final class StringUtil {
     return buffer.toString();
   }
 
+  /**
+   * Повторить строку n раз
+   * @param s строка
+   * @param n сколько повторять строку
+   * @return повторенная n раз строка
+   */
+  public static String repeat(String s, int n) {
+    if(s == null) return null;
+    if(n <= 0) return s;
+    final StringBuilder sb = new StringBuilder(s.length()*n);
+    for(int i = 0; i < n; i++) {
+        sb.append(s);
+    }
+    return sb.toString();
+  }
+
 }
