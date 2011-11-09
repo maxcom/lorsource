548ff33ae72518e64f27936e493d4fe5af04424b
Максим Валянский <max.valjanski@gmail.com>
2011-11-09 08:51:13 +0400

    StatUpdater: добавил удаление старых уведомлений - не более 10000 на юзера

--- src/main/java/ru/org/linux/spring/StatUpdater.java
+++ src/main/java/ru/org/linux/spring/StatUpdater.java
@@ -15,28 +15,38 @@
 
 package ru.org.linux.spring;
 
-import javax.sql.DataSource;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class StatUpdater {
   private static final Log logger = LogFactory.getLog(StatUpdater.class);
+  private static final int MAX_EVENTS = 10000;
 
   private SimpleJdbcCall statUpdate;
   private SimpleJdbcCall statUpdate2;
   private SimpleJdbcCall statMonthly;
+
+  private JdbcTemplate jdbcTemplate;
 
   @Autowired
   public void setDataSource(DataSource dataSource) {
     statUpdate = new SimpleJdbcCall(dataSource).withFunctionName("stat_update");
     statUpdate2 = new SimpleJdbcCall(dataSource).withFunctionName("stat_update2");
     statMonthly = new SimpleJdbcCall(dataSource).withFunctionName("update_monthly_stats");
+    jdbcTemplate = new JdbcTemplate(dataSource);
   }
 
   @Scheduled(fixedDelay=10*60*1000)
@@ -46,5 +56,32 @@ public class StatUpdater {
     statUpdate.execute();
     statUpdate2.execute();
     statMonthly.execute();
+  }
+
+  @Scheduled(fixedDelay = 60*60*1000)
+  public void creanEvents() {
+    final List<Integer> deleteList = new ArrayList<Integer>();
+
+    jdbcTemplate.query(
+            "select userid, count(user_events.id) from user_events group by userid order by count desc limit 10",
+            new RowCallbackHandler() {
+              @Override
+              public void processRow(ResultSet rs) throws SQLException {
+                if (rs.getInt("count")>MAX_EVENTS) {
+                  deleteList.add(rs.getInt("userid"));
+                }
+              }
+            }
+    );
+
+    for (int id : deleteList) {
+      logger.info("Cleaning messages for userid="+id);
+
+      jdbcTemplate.update(
+              "DELETE FROM user_events WHERE user_events.id IN (SELECT id FROM user_events WHERE userid=? ORDER BY event_date DESC OFFSET ?)",
+              id,
+              MAX_EVENTS
+      );
+    }
   }
 }
