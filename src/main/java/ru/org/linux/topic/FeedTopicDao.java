362eb007ae50f31bf500c697b04f80e69591e453
Maxim Valyanskiy <max.valjanski@gmail.com>
2012-01-30 17:48:17 +0400

    FeedTopicDao: bad sql grammar в архиве, см. #178

--- src/main/java/ru/org/linux/topic/FeedTopicDao.java
+++ src/main/java/ru/org/linux/topic/FeedTopicDao.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2012 Linux.org.ru
+ * Copyright 1998-2010 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -196,7 +196,7 @@ public class FeedTopicDao {
           .append(dateFormat.format(feedTopicDto.getFromDate()))
           .append("'::timestamp AND postdate<'")
           .append(dateFormat.format(feedTopicDto.getToDate()))
-          .append("::timestamp ");
+          .append("'::timestamp ");
         break;
       case MONTH_AGO:
         where
