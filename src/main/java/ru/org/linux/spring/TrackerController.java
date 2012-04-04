5446053c66bda9283ca63a6fc39011c0b73d57f6
Maxim Valyanskiy <max.valjanski@gmail.com>
2012-04-04 08:57:46 +0400

    tracker: более правильный redirect, cleanup

--- src/main/java/ru/org/linux/spring/TrackerController.java
+++ src/main/java/ru/org/linux/spring/TrackerController.java
@@ -15,15 +15,14 @@
 
 package ru.org.linux.spring;
 
-import com.google.common.base.Function;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
 import ru.org.linux.site.Template;
 import ru.org.linux.spring.dao.DeleteInfoDao;
 import ru.org.linux.spring.dao.TrackerDao;
@@ -32,6 +31,8 @@ import ru.org.linux.user.UserDao;
 import ru.org.linux.user.UserErrorException;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.sql.Timestamp;
 import java.util.*;
 
@@ -73,21 +74,33 @@ public class TrackerController {
   }
 
   @RequestMapping("/tracker.jsp")
-  public String tracker() {
-    return "redirect:tracker";
+  public View trackerOldUrl(
+          @RequestParam(value="filter", defaultValue = "all") String filterAction
+  ) throws UnsupportedEncodingException {
+    RedirectView redirectView = new RedirectView("/tracker/");
+
+    redirectView.setExposeModelAttributes(false);
+
+    if (filterValues.contains(filterAction) && !filterAction.equals("all")) {
+      redirectView.setUrl("/tracker/?filter="+ URLEncoder.encode(filterAction, "UTF-8"));
+    }
+
+    return redirectView;
+  }
+
+  private TrackerFilterEnum getFilterValue(String filterAction) {
+    if(filterValues.contains(filterAction)) {
+      return TrackerFilterEnum.valueOf(filterAction.toUpperCase());
+    } else {
+      return TrackerFilterEnum.ALL;
+    }
   }
 
   @RequestMapping("/tracker")
   public ModelAndView tracker(
-      @ModelAttribute("tracker") TrackerFilterAction action,
+      @RequestParam(value="filter", defaultValue = "all") String filterAction,
       @RequestParam(value="offset", required = false) Integer offset,
       HttpServletRequest request) throws Exception {
-
-    if (action.getFilter()==null) {
-      action.setFilter(TrackerFilterEnum.ALL.getValue());
-    }
-
-    String filterAction = action.getFilter();
 
     if (offset==null) {
       offset = 0;
@@ -97,12 +110,7 @@ public class TrackerController {
       }
     }
 
-    TrackerFilterEnum trackerFilter;
-    if(filterValues.contains(filterAction)) {
-      trackerFilter = TrackerFilterEnum.valueOf(filterAction.toUpperCase());
-    } else {
-      trackerFilter = TrackerFilterEnum.ALL;
-    }
+    TrackerFilterEnum trackerFilter = getFilterValue(filterAction);
 
     Map<String, Object> params = new HashMap<String, Object>();
     params.put("mine", trackerFilter == TrackerFilterEnum.MINE);
