3a353a274659cdf10b91dc74dea25de173cfcd34
Maxim Valyanskiy <max.valjanski@gmail.com>
2012-04-19 09:11:12 +0400

    LoginController: корректная обработка UserNotFoundException

--- src/main/java/ru/org/linux/auth/LoginController.java
+++ src/main/java/ru/org/linux/auth/LoginController.java
@@ -27,6 +27,7 @@ import ru.org.linux.spring.Configuration;
 import ru.org.linux.user.User;
 import ru.org.linux.user.UserBanedException;
 import ru.org.linux.user.UserDao;
+import ru.org.linux.user.UserNotFoundException;
 import ru.org.linux.util.StringUtil;
 
 import javax.servlet.http.Cookie;
@@ -193,8 +194,17 @@ public class LoginController {
    */
   @ExceptionHandler(UserBanedException.class)
   @ResponseStatus(HttpStatus.FORBIDDEN)
-  public ModelAndView handleUserBanedException(UserBanedException ex, HttpServletRequest request, HttpServletResponse response) {
+  public ModelAndView handleUserBanedException(UserBanedException ex) {
     return new ModelAndView("errors/user-banned", "exception", ex);
   }
 
+  /**
+   * Обрабатываем исключительную ситуацию для забаненого пользователя
+   */
+  @ExceptionHandler(UserNotFoundException.class)
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  public ModelAndView handleUserNotFoundException(HttpServletRequest request) {
+    boolean ajax = isAjax(request);
+    return new ModelAndView(ajax ? "login-xml" : "login-form", Collections.singletonMap("error", "Неверный пароль"));
+  }
 }
