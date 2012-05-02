b59475589763efb3f0e606c33115bc1b6709029f
Slava Zanko <slavazanko@gmail.com>
2012-05-02 22:24:53 +0300

    Рефакторинг: создание CommentService

--- src/main/java/ru/org/linux/comment/AddCommentController.java
+++ src/main/java/ru/org/linux/comment/AddCommentController.java
@@ -27,54 +27,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
-import ru.org.linux.auth.CaptchaService;
-import ru.org.linux.auth.FloodProtector;
 import ru.org.linux.auth.IPBlockDao;
 import ru.org.linux.auth.IPBlockInfo;
-import ru.org.linux.search.SearchQueueSender;
-import ru.org.linux.site.MessageNotFoundException;
 import ru.org.linux.site.Template;
-import ru.org.linux.topic.Topic;
-import ru.org.linux.topic.TopicDao;
 import ru.org.linux.topic.TopicPermissionService;
 import ru.org.linux.topic.TopicPrepareService;
 import ru.org.linux.user.User;
 import ru.org.linux.user.UserDao;
-import ru.org.linux.user.UserNotFoundException;
-import ru.org.linux.user.UserPropertyEditor;
-import ru.org.linux.util.ExceptionBindingErrorProcessor;
 import ru.org.linux.util.ServletParameterException;
 import ru.org.linux.util.StringUtil;
 import ru.org.linux.util.bbcode.LorCodeService;
-import ru.org.linux.util.formatter.ToLorCodeFormatter;
-import ru.org.linux.util.formatter.ToLorCodeTexFormatter;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.beans.PropertyEditorSupport;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 @Controller
 public class AddCommentController {
   private static final Log logger = LogFactory.getLog(AddCommentController.class);
 
   @Autowired
-  private SearchQueueSender searchQueueSender;
-
-  @Autowired
-  private CaptchaService captcha;
-
-  @Autowired
-  private FloodProtector dupeProtector;
-
-  @Autowired
   private CommentDao commentDao;
-
-  @Autowired
-  private TopicDao messageDao;
 
   @Autowired
   private UserDao userDao;
@@ -83,19 +57,16 @@ public class AddCommentController {
   private IPBlockDao ipBlockDao;
 
   @Autowired
-  private CommentPrepareService prepareService;
+  private CommentPrepareService commentPrepareService;
 
   @Autowired
   private LorCodeService lorCodeService;
 
   @Autowired
-  private ToLorCodeFormatter toLorCodeFormatter;
+  private CommentService commentService;
 
   @Autowired
-  private ToLorCodeTexFormatter toLorCodeTexFormatter;
-
-  @Autowired
-  private TopicPermissionService permissionService;
+  private TopicPermissionService topicPermissionService;
 
   @Autowired
   private TopicPrepareService messagePrepareService;
@@ -105,20 +76,20 @@ public class AddCommentController {
     @ModelAttribute("add") @Valid CommentRequest add,
     HttpServletRequest request
   ) throws Exception {
-    if (add.getTopic()==null) {
+    if (add.getTopic() == null) {
       throw new ServletParameterException("тема на задана");
     }
 
     Template tmpl = Template.getTemplate(request);
-    
+
     Map<String, Object> params = new HashMap<String, Object>();
 
-    if (add.getMode()==null) {
+    if (add.getMode() == null) {
       add.setMode(tmpl.getFormatMode());
     }
 
-    prepareReplyto(add, params, request);
-    
+    commentService.prepareReplyto(add, params, request);
+
     params.put("postscoreInfo", TopicPermissionService.getPostScoreInfo(add.getTopic().getPostScore()));
 
     return new ModelAndView("add_comment", params);
@@ -131,7 +102,7 @@ public class AddCommentController {
   ) {
     Template tmpl = Template.getTemplate(request);
 
-    if (add.getMode()==null) {
+    if (add.getMode() == null) {
       add.setMode(tmpl.getFormatMode());
     }
 
@@ -146,188 +117,56 @@ public class AddCommentController {
     return modelAndView;
   }
 
-  private String processMessage(String msg, String mode) {
-    if("ntobr".equals(mode)) {
-      return toLorCodeFormatter.format(msg, true);
-    } else {
-      return toLorCodeTexFormatter.format(msg);
-    }
-  }
-
   @RequestMapping(value = "/add_comment.jsp", method = RequestMethod.POST)
   public ModelAndView addComment(
     @ModelAttribute("add") @Valid CommentRequest add,
     Errors errors,
     HttpServletRequest request
   ) throws Exception {
-    Template tmpl = Template.getTemplate(request);
-
-    if (add.getMsg()==null) {
-      errors.rejectValue("msg", null, "комментарий не задан");
-      add.setMsg("");
-    }
-
-    if (add.getMode()==null) {
-      add.setMode(tmpl.getFormatMode());
-    }
-
-    String msg = processMessage(add.getMsg(), add.getMode());
-
-    HttpSession session = request.getSession();
-
-    IPBlockInfo ipBlockInfo = ipBlockDao.getBlockInfo(request.getRemoteAddr());
-
-    if (!add.isPreviewMode() &&
-      (!tmpl.isSessionAuthorized() || ipBlockInfo.isCaptchaRequired())) {
-      captcha.checkCaptcha(request, errors);
-    }
-
-    if (!add.isPreviewMode() && !errors.hasErrors() && !session.getId().equals(request.getParameter("session"))) {
-      logger.info(String.format(
-              "Flood protection (session variable differs: session=%s var=%s) ip=%s",
-              session.getId(),
-              request.getParameter("session"),
-              request.getRemoteAddr()
-      ));
-
-      errors.reject(null, "сбой добавления, попробуйте еще раз");
-    }
 
     Map<String, Object> formParams = new HashMap<String, Object>();
 
-    prepareReplyto(add, formParams, request);
+    User user = commentService.getCommentUser(add, request, errors);
+    IPBlockInfo ipBlockInfo = ipBlockDao.getBlockInfo(request.getRemoteAddr());
 
-    User user;
+    commentService.checkPostData(add, user, ipBlockInfo, request, errors);
+    commentService.prepareReplyto(add, formParams, request);
 
-    if (!tmpl.isSessionAuthorized()) {
-      if (add.getNick() != null) {
-        user = add.getNick();
-      } else {
-        user = userDao.getAnonymous();
-      }
+    String msg = commentService.getCommentBody(add, user, errors);
+    Comment comment = commentService.getComment(add, user, request);
 
-      if (add.getPassword()==null) {
-        errors.reject(null, "Требуется авторизация");
-      }
-    } else {
-      user = tmpl.getCurrentUser();
-    }
-
-    user.checkBlocked(errors);
-
-    if (ipBlockInfo.isBlocked()) {
-      ipBlockDao.checkBlockIP(ipBlockInfo, errors, user);
-    }
-
-    if (user.isAnonymous()) {
-      if (msg.length() > 4096) {
-        errors.rejectValue("msg", null, "Слишком большое сообщение");
-      }
-    } else {
-      if (msg.length() > 8192) {
-        errors.rejectValue("msg", null, "Слишком большое сообщение");
-      }
-    }
-
-    Comment comment = null;
-
-    if (add.getTopic()!=null) {
+    if (add.getTopic() != null) {
       formParams.put("postscoreInfo", TopicPermissionService.getPostScoreInfo(add.getTopic().getPostScore()));
 
-      permissionService.checkCommentsAllowed(add.getTopic(), user, errors);
-
-      String title = add.getTitle();
-
-      if (title==null) {
-        title="";
-      }
-
-      Integer replyto = add.getReplyto()!=null?add.getReplyto().getId():null;
-
-      comment = new Comment(
-              replyto,
-              StringUtil.escapeHtml(title),
-              add.getTopic().getId(),
-              user.getId(),
-              request.getHeader("user-agent"),
-              request.getRemoteAddr()
-      );
-
-      formParams.put("comment", prepareService.prepareComment(comment, msg, request.isSecure()));
+      topicPermissionService.checkCommentsAllowed(add.getTopic(), user, errors);
+      formParams.put("comment", commentPrepareService.prepareComment(comment, msg, request.isSecure()));
     }
 
-    if (!add.isPreviewMode() && !errors.hasErrors()) {
-      dupeProtector.checkDuplication(request.getRemoteAddr(), user.getScore() > 100, errors);
+    if (add.isPreviewMode() || errors.hasErrors() || comment == null) {
+      ModelAndView modelAndView = new ModelAndView("add_comment", formParams);
+      modelAndView.addObject("ipBlockInfo", ipBlockInfo);
+      add.setMsg(StringUtil.escapeForceHtml(add.getMsg()));
+      return modelAndView;
     }
 
-    if (!add.isPreviewMode() && !errors.hasErrors() && comment != null) {
-      Set<User> userRefs = lorCodeService.getReplierFromMessage(msg);
+    int msgid = commentService.create(
+      comment,
+      msg,
+      request.getRemoteAddr(),
+      request.getHeader("X-Forwarded-For")
+    );
 
-      int msgid = commentDao.saveNewMessage(comment, msg, userRefs);
-
-      String logmessage = "Написан комментарий " + msgid + " ip:" + request.getRemoteAddr();
-      if (request.getHeader("X-Forwarded-For") != null) {
-        logmessage = logmessage + " XFF:" + request.getHeader(("X-Forwarded-For"));
-      }
-
-      logger.info(logmessage);
-
-      // TODO надо разобраться с транзакциями и засунуть это в saveNewMessage
-      searchQueueSender.updateComment(msgid);
-
-      String returnUrl = "jump-message.jsp?msgid=" + add.getTopic().getId() + "&cid=" + msgid;
-
-      return new ModelAndView(new RedirectView(returnUrl));
-    } else {
-          add.setMsg(StringUtil.escapeForceHtml(add.getMsg()));
-    }
-    ModelAndView modelAndView = new ModelAndView("add_comment", formParams);
-    modelAndView.addObject("ipBlockInfo", ipBlockInfo);
-    return modelAndView;
-  }
-
-  private void prepareReplyto(CommentRequest add, Map<String, Object> formParams, HttpServletRequest request) throws UserNotFoundException {
-    if (add.getReplyto()!=null) {
-      formParams.put("onComment", prepareService.prepareComment(add.getReplyto(), request.isSecure()));
-    }
+    String returnUrl = "jump-message.jsp?msgid=" + add.getTopic().getId() + "&cid=" + msgid;
+    return new ModelAndView(new RedirectView(returnUrl));
   }
 
   @InitBinder("add")
   public void requestValidator(WebDataBinder binder) {
-    binder.setValidator(new CommentRequestValidator());
-
-    binder.setBindingErrorProcessor(new ExceptionBindingErrorProcessor());
+    commentService.requestValidator(binder);
   }
 
   @InitBinder
   public void initBinder(WebDataBinder binder) {
-    binder.registerCustomEditor(Topic.class, new PropertyEditorSupport() {
-      @Override
-      public void setAsText(String text) throws IllegalArgumentException {
-        try {
-          setValue(messageDao.getById(Integer.parseInt(text)));
-        } catch (MessageNotFoundException e) {
-          throw new IllegalArgumentException(e);
-        }
-      }
-    });
-
-    binder.registerCustomEditor(Comment.class, new PropertyEditorSupport() {
-      @Override
-      public void setAsText(String text) throws IllegalArgumentException {
-        if (text.isEmpty() || "0".equals(text)) {
-          setValue(null);
-          return;
-        }
-
-        try {
-          setValue(commentDao.getById(Integer.parseInt(text)));
-        } catch (MessageNotFoundException e) {
-          throw new IllegalArgumentException(e);
-        }
-      }
-    });
-
-    binder.registerCustomEditor(User.class, new UserPropertyEditor(userDao));
+    commentService.initBinder(binder);
   }
 }
---
+++ src/main/java/ru/org/linux/comment/CommentService.java
@@ -0,0 +1,384 @@
+/*
+ * Copyright 1998-2012 Linux.org.ru
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package ru.org.linux.comment;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ApplicationObjectSupport;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.xbill.DNS.TextParseException;
+import ru.org.linux.auth.CaptchaService;
+import ru.org.linux.auth.FloodProtector;
+import ru.org.linux.auth.IPBlockDao;
+import ru.org.linux.auth.IPBlockInfo;
+import ru.org.linux.search.SearchQueueSender;
+import ru.org.linux.site.MessageNotFoundException;
+import ru.org.linux.site.Template;
+import ru.org.linux.spring.dao.MessageText;
+import ru.org.linux.spring.dao.MsgbaseDao;
+import ru.org.linux.topic.Topic;
+import ru.org.linux.topic.TopicDao;
+import ru.org.linux.user.IgnoreListDao;
+import ru.org.linux.user.User;
+import ru.org.linux.user.UserDao;
+import ru.org.linux.user.UserEventService;
+import ru.org.linux.user.UserNotFoundException;
+import ru.org.linux.user.UserPropertyEditor;
+import ru.org.linux.util.ExceptionBindingErrorProcessor;
+import ru.org.linux.util.ServletParameterException;
+import ru.org.linux.util.StringUtil;
+import ru.org.linux.util.bbcode.LorCodeService;
+import ru.org.linux.util.formatter.ToLorCodeFormatter;
+import ru.org.linux.util.formatter.ToLorCodeTexFormatter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.beans.PropertyEditorSupport;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Service
+public class CommentService {
+  private static final Log logger = LogFactory.getLog(CommentService.class);
+
+  @Autowired
+  private CommentDao commentDao;
+
+  @Autowired
+  private TopicDao messageDao;
+
+  @Autowired
+  private UserDao userDao;
+
+  @Autowired
+  private ToLorCodeFormatter toLorCodeFormatter;
+
+  @Autowired
+  private ToLorCodeTexFormatter toLorCodeTexFormatter;
+
+  @Autowired
+  private CaptchaService captcha;
+
+  @Autowired
+  private CommentPrepareService commentPrepareService;
+
+  @Autowired
+  private FloodProtector floodProtector;
+
+  @Autowired
+  private IPBlockDao ipBlockDao;
+
+  @Autowired
+  private LorCodeService lorCodeService;
+
+  @Autowired
+  private UserEventService userEventService;
+
+  @Autowired
+  private MsgbaseDao msgbaseDao;
+
+  @Autowired
+  private SearchQueueSender searchQueueSender;
+
+  @Autowired
+  private IgnoreListDao ignoreListDao;
+
+  /**
+   * @param binder
+   */
+  public void requestValidator(WebDataBinder binder) {
+    binder.setValidator(new CommentRequestValidator());
+    binder.setBindingErrorProcessor(new ExceptionBindingErrorProcessor());
+  }
+
+  /**
+   * @param binder
+   */
+  public void initBinder(WebDataBinder binder) {
+    binder.registerCustomEditor(Topic.class, new PropertyEditorSupport() {
+      @Override
+      public void setAsText(String text) throws IllegalArgumentException {
+        try {
+          setValue(messageDao.getById(Integer.parseInt(text.split(",")[0])));
+        } catch (MessageNotFoundException e) {
+          throw new IllegalArgumentException(e);
+        }
+      }
+    });
+
+    binder.registerCustomEditor(Comment.class, new PropertyEditorSupport() {
+      @Override
+      public void setAsText(String text) throws IllegalArgumentException {
+        if (text.isEmpty() || "0".equals(text)) {
+          setValue(null);
+          return;
+        }
+
+        try {
+          setValue(commentDao.getById(Integer.parseInt(text)));
+        } catch (MessageNotFoundException e) {
+          throw new IllegalArgumentException(e);
+        }
+      }
+    });
+
+    binder.registerCustomEditor(User.class, new UserPropertyEditor(userDao));
+  }
+
+  /**
+   * @param commentRequest
+   * @param user
+   * @param ipBlockInfo
+   * @param request
+   * @param errors
+   * @throws UserNotFoundException
+   * @throws UnknownHostException
+   * @throws TextParseException
+   */
+  public void checkPostData(
+    CommentRequest commentRequest,
+    User user,
+    IPBlockInfo ipBlockInfo,
+    HttpServletRequest request,
+    Errors errors
+  ) throws
+    UserNotFoundException,
+    UnknownHostException,
+    TextParseException {
+    if (commentRequest.getMsg() == null) {
+      errors.rejectValue("msg", null, "комментарий не задан");
+      commentRequest.setMsg("");
+    }
+
+    Template tmpl = Template.getTemplate(request);
+
+    if (commentRequest.getMode() == null) {
+      commentRequest.setMode(tmpl.getFormatMode());
+    }
+
+    HttpSession session = request.getSession();
+
+    if (!commentRequest.isPreviewMode() &&
+      (!tmpl.isSessionAuthorized() || ipBlockInfo.isCaptchaRequired())) {
+      captcha.checkCaptcha(request, errors);
+    }
+
+    if (!commentRequest.isPreviewMode() && !errors.hasErrors() && !session.getId().equals(request.getParameter("session"))) {
+      logger.info(String.format(
+        "Flood protection (session variable differs: session=%s var=%s) ip=%s",
+        session.getId(),
+        request.getParameter("session"),
+        request.getRemoteAddr()
+      ));
+
+      errors.reject(null, "сбой добавления, попробуйте еще раз");
+    }
+
+    user.checkBlocked(errors);
+
+    if (ipBlockInfo.isBlocked()) {
+      ipBlockDao.checkBlockIP(ipBlockInfo, errors, user);
+    }
+
+    if (!commentRequest.isPreviewMode() && !errors.hasErrors()) {
+      floodProtector.checkDuplication(request.getRemoteAddr(), user.getScore() > 100, errors);
+    }
+  }
+
+  /**
+   * @param commentRequest
+   * @param user
+   * @param errors
+   * @return
+   */
+  public String getCommentBody(
+    CommentRequest commentRequest,
+    User user,
+    Errors errors
+  ) {
+    String commentBody = processMessage(commentRequest.getMsg(), commentRequest.getMode());
+    if (user.isAnonymous()) {
+      if (commentBody.length() > 4096) {
+        errors.rejectValue("msg", null, "Слишком большое сообщение");
+      }
+    } else {
+      if (commentBody.length() > 8192) {
+        errors.rejectValue("msg", null, "Слишком большое сообщение");
+      }
+    }
+    return commentBody;
+  }
+
+  /**
+   * @param commentRequest
+   * @param user
+   * @param request
+   * @return
+   */
+  public Comment getComment(
+    CommentRequest commentRequest,
+    User user,
+    HttpServletRequest request
+  ) {
+    Comment comment = null;
+
+    if (commentRequest.getTopic() != null) {
+
+      String title = commentRequest.getTitle();
+
+      if (title == null) {
+        title = "";
+      }
+
+      Integer replyto = commentRequest.getReplyto() != null ? commentRequest.getReplyto().getId() : null;
+
+      comment = new Comment(
+        replyto,
+        StringUtil.escapeHtml(title),
+        commentRequest.getTopic().getId(),
+        user.getId(),
+        request.getHeader("user-agent"),
+        request.getRemoteAddr()
+      );
+    }
+    return comment;
+  }
+
+  /**
+   * @param commentRequest
+   * @param request
+   * @param errors
+   * @return
+   */
+  public User getCommentUser(
+    CommentRequest commentRequest,
+    HttpServletRequest request,
+    Errors errors
+  ) {
+    User user;
+
+    Template tmpl = Template.getTemplate(request);
+
+    if (!tmpl.isSessionAuthorized()) {
+      if (commentRequest.getNick() != null) {
+        user = commentRequest.getNick();
+      } else {
+        user = userDao.getAnonymous();
+      }
+
+      if (commentRequest.getPassword() == null) {
+        errors.reject(null, "Требуется авторизация");
+      }
+    } else {
+      user = tmpl.getCurrentUser();
+    }
+    return user;
+  }
+
+  /**
+   * @param add
+   * @param formParams
+   * @param request
+   * @throws UserNotFoundException
+   */
+  public void prepareReplyto(
+    CommentRequest add,
+    Map<String, Object> formParams,
+    HttpServletRequest request
+  ) throws UserNotFoundException {
+    if (add.getReplyto() != null) {
+      formParams.put("onComment", commentPrepareService.prepareComment(add.getReplyto(), request.isSecure()));
+    }
+  }
+
+  /**
+   * Создание нового комментария.
+   *
+   * @param comment
+   * @param commentBody
+   * @param remoteAddress
+   * @param xForwardedFor
+   * @return
+   * @throws MessageNotFoundException
+   */
+  @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
+  public int create(
+    Comment comment,
+    String commentBody,
+    String remoteAddress,
+    String xForwardedFor
+  ) throws MessageNotFoundException {
+
+    int commentId = commentDao.saveNewMessage(comment, commentBody);
+
+    /* кастование пользователей */
+    Set<User> userRefs = lorCodeService.getReplierFromMessage(commentBody);
+    userEventService.addUserRefEvent(userRefs.toArray(new User[userRefs.size()]), comment.getTopicId(), commentId);
+
+    /* оповещение об ответе на коммент */
+    if (comment.getReplyTo() != 0) {
+      try {
+        Comment parentComment = commentDao.getById(comment.getReplyTo());
+
+        if (parentComment.getUserid() != comment.getUserid()) {
+          User parentAuthor = userDao.getUserCached(parentComment.getUserid());
+
+          if (!parentAuthor.isAnonymous()) {
+            Set<Integer> ignoreList = ignoreListDao.get(parentAuthor);
+
+            if (!ignoreList.contains(comment.getUserid())) {
+              userEventService.addReplyEvent(
+                parentAuthor,
+                comment.getTopicId(),
+                commentId
+              );
+            }
+          }
+        }
+      } catch (UserNotFoundException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    String logmessage = "Написан комментарий " + commentId + " ip:" + remoteAddress;
+    if (xForwardedFor != null) {
+      logmessage = logmessage + " XFF:" + xForwardedFor;
+    }
+
+    logger.info(logmessage);
+
+    // TODO надо разобраться с транзакциями и засунуть это в saveNewMessage
+    searchQueueSender.updateComment(commentId);
+
+    return commentId;
+  }
+
+  private String processMessage(String msg, String mode) {
+    if ("ntobr".equals(mode)) {
+      return toLorCodeFormatter.format(msg, true);
+    } else {
+      return toLorCodeTexFormatter.format(msg);
+    }
+  }
+
+}
