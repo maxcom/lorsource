1e4c4d595a16bf89868e25a70fc371d7ae2a98f0
Maxim Valyanskiy <max.valjanski@gmail.com>
2012-05-11 17:35:01 +0400

    AddCommentController: @Autowired прямо на поля, удалил setter'ы

--- src/main/java/ru/org/linux/comment/AddCommentController.java
+++ src/main/java/ru/org/linux/comment/AddCommentController.java
@@ -58,16 +58,37 @@ import java.util.Set;
 
 @Controller
 public class AddCommentController extends ApplicationObjectSupport {
+  @Autowired
   private SearchQueueSender searchQueueSender;
+
+  @Autowired
   private CaptchaService captcha;
+
+  @Autowired
   private FloodProtector dupeProtector;
+
+  @Autowired
   private CommentDao commentDao;
+
+  @Autowired
   private TopicDao messageDao;
+
+  @Autowired
   private UserDao userDao;
+
+  @Autowired
   private IPBlockDao ipBlockDao;
+
+  @Autowired
   private CommentPrepareService prepareService;
+
+  @Autowired
   private LorCodeService lorCodeService;
+
+  @Autowired
   private ToLorCodeFormatter toLorCodeFormatter;
+
+  @Autowired
   private ToLorCodeTexFormatter toLorCodeTexFormatter;
 
   @Autowired
@@ -75,61 +96,6 @@ public class AddCommentController extends ApplicationObjectSupport {
 
   @Autowired
   private TopicPrepareService messagePrepareService;
-
-  @Autowired
-  public void setSearchQueueSender(SearchQueueSender searchQueueSender) {
-    this.searchQueueSender = searchQueueSender;
-  }
-
-  @Autowired
-  public void setCaptcha(CaptchaService captcha) {
-    this.captcha = captcha;
-  }
-
-  @Autowired
-  public void setDupeProtector(FloodProtector dupeProtector) {
-    this.dupeProtector = dupeProtector;
-  }
-
-  @Autowired
-  public void setCommentDao(CommentDao commentDao) {
-    this.commentDao = commentDao;
-  }
-
-  @Autowired
-  public void setMessageDao(TopicDao messageDao) {
-    this.messageDao = messageDao;
-  }
-
-  @Autowired
-  public void setUserDao(UserDao userDao) {
-    this.userDao = userDao;
-  }
-
-  @Autowired
-  public void setIpBlockDao(IPBlockDao ipBlockDao) {
-    this.ipBlockDao = ipBlockDao;
-  }
-
-  @Autowired
-  public void setPrepareService(CommentPrepareService prepareService) {
-    this.prepareService = prepareService;
-  }
-
-  @Autowired
-  public void setLorCodeService(LorCodeService lorCodeService) {
-    this.lorCodeService = lorCodeService;
-  }
-
-  @Autowired
-  public void setToLorCodeFormatter(ToLorCodeFormatter toLorCodeFormatter) {
-    this.toLorCodeFormatter = toLorCodeFormatter;
-  }
-
-  @Autowired
-  public void setToLorCodeTexFormatter(ToLorCodeTexFormatter toLorCodeTexFormatter) {
-    this.toLorCodeTexFormatter = toLorCodeTexFormatter;
-  }
 
   @RequestMapping(value = "/add_comment.jsp", method = RequestMethod.GET)
   public ModelAndView showFormReply(
