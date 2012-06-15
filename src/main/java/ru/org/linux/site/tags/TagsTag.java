f00a4d4e31533b1203a117c6a204ac2bb9114094
Ildar Hizbulin <hizel@vyborg.ru>
2012-06-15 12:01:16 +0400

    reformat and copyright TagsTag code

--- src/main/java/ru/org/linux/site/tags/TagsTag.java
+++ src/main/java/ru/org/linux/site/tags/TagsTag.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 1998-2012 Linux.org.ru
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package ru.org.linux.site.tags;
 
 import ru.org.linux.tag.TagService;
@@ -14,42 +28,42 @@ import java.util.List;
  * tags tag
  */
 public class TagsTag extends TagSupport {
-    private List<String> list;
+  private List<String> list;
 
-    public void setList(List<String> list) {
-        this.list = list;
-    }
+  public void setList(List<String> list) {
+    this.list = list;
+  }
 
 
-    @Override
-    public int doStartTag() throws JspException {
-        JspWriter out = pageContext.getOut();
-        if(list != null) {
-            try {
-                StringBuilder buf = new StringBuilder();
-                for(String el : list) {
-                    if(buf.length() > 0) {
-                        buf.append(", ");
-                    }
-                    if(TagService.isGoodTag(el)) {
-                        buf
-                            .append("<a class=tag rel=tag href=\"")
-                            .append(TopicListController.tagListUrl(el))
-                            .append("\">")
-                            .append(StringUtil.escapeHtml(el))
-                            .append("</a>");
-                    } else {
-                        buf.append(StringUtil.escapeHtml(el));
-                    }
-                }
-                out
-                    .append("<p class=\"tags\">Метки: ")
-                    .append(buf)
-                    .append("</p>");
-            } catch (IOException e) {
-                throw new JspException("Error:" + e.getMessage());
-            }
+  @Override
+  public int doStartTag() throws JspException {
+    JspWriter out = pageContext.getOut();
+    if (list != null) {
+      try {
+        StringBuilder buf = new StringBuilder();
+        for (String el : list) {
+          if (buf.length() > 0) {
+            buf.append(", ");
+          }
+          if (TagService.isGoodTag(el)) {
+            buf
+                .append("<a class=tag rel=tag href=\"")
+                .append(TopicListController.tagListUrl(el))
+                .append("\">")
+                .append(StringUtil.escapeHtml(el))
+                .append("</a>");
+          } else {
+            buf.append(StringUtil.escapeHtml(el));
+          }
         }
-        return SKIP_BODY;
+        out
+            .append("<p class=\"tags\">Метки: ")
+            .append(buf)
+            .append("</p>");
+      } catch (IOException e) {
+        throw new JspException("Error:" + e.getMessage());
+      }
     }
+    return SKIP_BODY;
+  }
 }
