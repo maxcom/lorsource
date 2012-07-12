0973ffb901f68100ac67420e1f3468d6860a39a0
Ildar Hizbulin <hizel@vyborg.ru>
2012-07-12 13:57:05 +0400

    в предыдущем коммите забыли новый файлик

---
+++ src/main/java/ru/org/linux/poll/PreparedPollVariantResult.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 1998-2012 Linux.org.ru
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package ru.org.linux.poll;
+
+/**
+ */
+public class PreparedPollVariantResult extends PollVariantResult {
+  final int percentage;
+  final int width;
+  final String alt;
+
+  public PreparedPollVariantResult(int id, String label, int votes, boolean userVoted,
+                                   int percentage, int width, String alt) {
+    super(id, label, votes, userVoted);
+    this.percentage = percentage;
+    this.width = width;
+    this.alt = alt;
+  }
+
+  public int getPercentage() {
+    return percentage;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public String getAlt() {
+    return alt;
+  }
+}
