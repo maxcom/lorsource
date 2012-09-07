bd6ad277f0618cbbd150c622b01589d94e7cd7c7
Ildar Hizbulin <hizel@vyborg.ru>
2012-09-07 14:43:42 +0400

    spring security: удаляем лишний оброботчик logout

--- src/main/java/ru/org/linux/auth/LoginController.java
+++ src/main/java/ru/org/linux/auth/LoginController.java
@@ -22,6 +22,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -29,26 +30,12 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
-import ru.org.linux.csrf.CSRFProtectionService;
-import ru.org.linux.site.BadInputException;
-import ru.org.linux.site.Template;
 import ru.org.linux.spring.Configuration;
-import ru.org.linux.user.User;
 import ru.org.linux.user.UserBanedException;
 import ru.org.linux.user.UserDao;
-import ru.org.linux.user.UserNotFoundException;
-import ru.org.linux.util.StringUtil;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.Collections;
 
 @Controller
 public class LoginController {
-  private static final Log logger = LogFactory.getLog(LoginController.class);
 
   public static final String ACEGI_COOKIE_NAME = "SPRING_SECURITY_REMEMBER_ME_COOKIE";
 
@@ -70,11 +57,13 @@ public class LoginController {
   public LoginStatus loginAjax(@RequestParam("nick") final String username, @RequestParam("passwd") final String password) {
     UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
     try {
-      UserDetailsImpl details = (UserDetailsImpl)userDetailsService.loadUserByUsername(username);
+      UserDetailsImpl details = (UserDetailsImpl) userDetailsService.loadUserByUsername(username);
       token.setDetails(details);
       Authentication auth = authenticationManager.authenticate(token);
       SecurityContextHolder.getContext().setAuthentication(auth);
       return new LoginStatus(auth.isAuthenticated(), auth.getName());
+    } catch (LockedException e) {
+      return new LoginStatus(false, "User locked");
     } catch (UsernameNotFoundException e) {
       return new LoginStatus(false, "Bad credentials");
     } catch (BadCredentialsException e) {
@@ -103,49 +92,6 @@ public class LoginController {
   @RequestMapping(value = "/login.jsp", method = RequestMethod.GET)
   public ModelAndView loginForm() {
     return new ModelAndView("login-form");
-  }
-
-  @RequestMapping(value = "/logout", method = {RequestMethod.GET, RequestMethod.POST})
-  public ModelAndView logout(
-          HttpServletRequest request,
-          HttpSession session,
-          HttpServletResponse response,
-          @RequestParam(required = false) String sessionId
-  ) {
-    Template tmpl = Template.getTemplate(request);
-
-    if (tmpl.isSessionAuthorized()) {
-      if (sessionId == null || !session.getId().equals(sessionId)) {
-        return new ModelAndView("logout");
-      }
-
-      session.removeAttribute("login");
-      session.removeAttribute("nick");
-      session.removeAttribute("moderator");
-      session.removeAttribute("ACEGI_SECURITY_CONTEXT"); // if any
-      Cookie cookie = new Cookie("password", "");
-      cookie.setMaxAge(60 * 60 * 24 * 31 * 24);
-      cookie.setPath("/");
-      response.addCookie(cookie);
-
-      Cookie cookie2 = new Cookie("profile", "");
-      cookie2.setMaxAge(60 * 60 * 24 * 31 * 24);
-      cookie2.setPath("/");
-      response.addCookie(cookie2);
-
-      Cookie cookie3 = new Cookie(ACEGI_COOKIE_NAME, "");
-      cookie3.setMaxAge(60 * 60 * 24 * 31 * 24);
-      cookie3.setPath("/wiki");
-      response.addCookie(cookie3);
-
-      // отчистка seesion id для wiki
-      Cookie cookie4 = new Cookie("JSESSIONID", "");
-      cookie4.setMaxAge(60 * 60 * 24 * 31 * 24);
-      cookie4.setPath("/wiki");
-      response.addCookie(cookie4);
-    }
-
-    return new ModelAndView(new RedirectView("/"));
   }
 
   /**
