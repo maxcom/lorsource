aef392189f1b05fa0163c0764bc1f637f19ba782
Yury <yfdrnchk@mail.ru>
2012-09-21 18:56:24 +0400

    заметки о пользователях

---
+++ src/main/java/ru/org/linux/user/EditRemarkController.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 1998-2012 Linux.org.ru
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package ru.org.linux.user;
+
+import javax.servlet.ServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.org.linux.auth.AccessViolationException;
+import ru.org.linux.site.Template;
+import ru.org.linux.site.BadInputException;
+import ru.org.linux.site.DefaultProfile;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/people/{nick}/remark")
+public class EditRemarkController {
+  private UserDao userDao;
+
+  @Autowired
+  public void setUserDao(UserDao userDao) {
+    this.userDao = userDao;
+  }
+
+  
+  @RequestMapping(method=RequestMethod.GET)
+  public ModelAndView showForm(ServletRequest request, @PathVariable String nick) throws Exception {
+    Template tmpl = Template.getTemplate(request);
+
+    if (!tmpl.isSessionAuthorized()) {
+      throw new AccessViolationException("Not authorized");
+    }
+    ModelAndView mv = new ModelAndView("edit-remark");
+
+    User user = userDao.getUser(nick);
+    mv.getModel().put("user",tmpl.getCurrentUser());
+    if (tmpl.isSessionAuthorized() && !tmpl.getNick().equals(nick) ) {
+      mv.getModel().put("remark", userDao.getRemarkClass(tmpl.getCurrentUser() , user) );
+    }else{
+      throw new AccessViolationException("Not Authorized");
+    }
+    return mv;
+  }
+
+  @RequestMapping(method=RequestMethod.POST)
+  public ModelAndView editProfile(
+          ServletRequest request,
+          @RequestParam("rid") String rid,
+          @RequestParam("user") String user_name,
+          @RequestParam("refuser") String ref_user_name,
+          @RequestParam("text") String text,
+          @PathVariable String nick
+  ) throws Exception {
+    Template tmpl = Template.getTemplate(request);
+
+    if (!tmpl.isSessionAuthorized()) {
+      throw new AccessViolationException("Not authorized");
+    }
+
+    if(!tmpl.getNick().equals(user_name)) {
+      throw new AccessViolationException("Not authorized");
+    }
+
+    if(text.length()>255){
+      //throw new AccessViolationException("No message to change"+rid);
+      text=text.substring(0,255);
+    }
+    User user = tmpl.getCurrentUser();
+    User ref_user = userDao.getUser(ref_user_name);
+    Remark rm = userDao.getRemarkClass(user,ref_user);
+    if(rm!=null){
+      userDao.updateRemark(rm.getId(),text);
+    } else {
+      userDao.setRemark(user,ref_user,text);
+    }
+    return new ModelAndView(new RedirectView("/people/" + nick + "/profile"));
+  }
+}
---
+++ src/main/webapp/WEB-INF/jsp/edit-remark.jsp
@@ -0,0 +1,45 @@
+<%@ page contentType="text/html; charset=utf-8"%>
+<%@ page import="ru.org.linux.site.DefaultProfile,ru.org.linux.site.Template" %>
+<%@ page import="ru.org.linux.user.ProfileProperties" %>
+<%--
+  ~ Copyright 1998-2012 Linux.org.ru
+  ~    Licensed under the Apache License, Version 2.0 (the "License");
+  ~    you may not use this file except in compliance with the License.
+  ~    You may obtain a copy of the License at
+  ~
+  ~        http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~    Unless required by applicable law or agreed to in writing, software
+  ~    distributed under the License is distributed on an "AS IS" BASIS,
+  ~    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  ~    See the License for the specific language governing permissions and
+  ~    limitations under the License.
+  --%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="lor" %>
+
+<%--@elvariable id="remark" type="ru.org.linux.user.Remark"--%>
+<%--@elvariable id="user" type="ru.org.linux.user.User"--%>
+
+<% Template tmpl = Template.getTemplate(request); %>
+
+<jsp:include page="/WEB-INF/jsp/head.jsp"/>
+
+<title>Комментарий о пользователе</title>
+
+<jsp:include page="/WEB-INF/jsp/header.jsp"/>
+
+<h1 class="optional">Комментарий ${user.nick} ${remark.id} о пользователе ${nick}</h1>
+
+<form method=POST id="remarkForm" action="/people/${nick}/remark">
+<input type=hidden id="rid" name="rid" value="${remark.id}" >
+<input type=hidden id="user" name="user" value="${user.nick}" >
+<input type=hidden id="refuser" name="refuser" value="${nick}" >
+<lor:csrf/>
+<textarea id="text" name="text" cols="60" rows="4">${remark.text}</textarea>
+<br>
+<input type=submit value="Установить">
+</form>
+
+
+<jsp:include page="/WEB-INF/jsp/footer.jsp"/>
