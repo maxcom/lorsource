3905e0f67b37106b3ea167af334d8224ffbdba66
Maxim Valyanskiy <max.valjanski@gmail.com>
2012-11-02 22:49:36 +0400

    debug для spring security в тестах

---
+++ src/test/java/ru/org/linux/topic/AddTopicControllerWebTest.java
@@ -0,0 +1,11 @@
+package ru.org.linux.topic;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: maxcom
+ * Date: 02.11.12
+ * Time: 20:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class AddTopicControllerWebTest {
+}
