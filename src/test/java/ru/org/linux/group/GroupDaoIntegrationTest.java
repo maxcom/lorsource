8f4b19c1e98c227290bde13478e1e5e06aab6be0
Slava Zanko <slavazanko@gmail.com>
2012-11-03 22:54:06 +0300

    Рефакторинг тестов: GroupDaoIntegrationTest

--- src/test/java/ru/org/linux/group/GroupDaoIntegrationTest.java
+++ src/test/java/ru/org/linux/group/GroupDaoIntegrationTest.java
@@ -18,6 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.org.linux.section.Section;
 
@@ -31,17 +33,69 @@ public class GroupDaoIntegrationTest extends AbstractTestNGSpringContextTests {
   @Autowired
 
   GroupDao groupDao;
+  private Section sectionDto;
+
+  @BeforeMethod
+  public void sectionDtoInit() {
+    sectionDto = mock(Section.class);
+    when(sectionDto.getId()).thenReturn(Section.SECTION_FORUM);
+  }
 
   @Test
-  public void groupsTest()
-      throws Exception {
-    Section sectionDto = mock(Section.class);
-    when(sectionDto.getId()).thenReturn(Section.SECTION_FORUM);
+  public void groupsCountInSectionForum() {
+    // given
 
+    // when
     List<Group> groupDtoList = groupDao.getGroups(sectionDto);
-    Assert.assertEquals(16, groupDtoList.size());
 
-    Group groupDto = groupDao.getGroup(sectionDto, "general");
-    Assert.assertEquals("General", groupDto.getTitle());
+    // then
+    Assert.assertEquals(16, groupDtoList.size());
   }
+
+  @Test(expectedExceptions = {BadGroupException.class})
+  public void badGroupName() throws BadGroupException {
+    // given
+
+    // when
+    groupDao.getGroup(sectionDto, "bad group name");
+
+    // then
+  }
+
+  @Test(dataProvider = "sectionForumHaveGroupNamedDataSource")
+  public void sectionForumHaveGroupNamed(
+    String inputGroupName,
+    String expectedGroupTitle
+  ) throws BadGroupException {
+    // given
+
+    // when
+    Group groupDto = groupDao.getGroup(sectionDto, inputGroupName);
+
+    // then
+    Assert.assertEquals(groupDto.getTitle(), expectedGroupTitle);
+  }
+
+  @DataProvider(name = "sectionForumHaveGroupNamedDataSource")
+  public Object[][] sectionForumHaveGroupNamedDataSource() {
+    return new Object[][]{
+      new Object[]{"admin", "Admin"},
+      new Object[]{"desktop", "Desktop"},
+      new Object[]{"development", "Development"},
+      new Object[]{"games", "Games"},
+      new Object[]{"general", "General"},
+      new Object[]{"job", "Job"},
+      new Object[]{"linux-hardware", "Linux-hardware"},
+      new Object[]{"linux-install", "Linux-install"},
+      new Object[]{"linux-org-ru", "Linux-org-ru"},
+      new Object[]{"lor-source", "Lor-source"},
+      new Object[]{"mobile", "Mobile"},
+      new Object[]{"multimedia", "Multimedia"},
+      new Object[]{"security", "Security"},
+      new Object[]{"talks", "Talks"},
+      new Object[]{"web-development", "Web-development"},
+      new Object[]{"club", "Клуб"}
+    };
+  }
+
 }
