612e9162c5a7ef84c84dd51df19cf9b88631917a
Maxim Valyanskiy <max.valjanski@gmail.com>
2012-11-07 19:18:00 +0400

    начал делать тест к UserDao

---
+++ src/test/java/ru/org/linux/user/UserDaoIntegrationTest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 1998-2012 Linux.org.ru
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package ru.org.linux.user;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.Cache;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+
+import static junit.framework.Assert.*;
+
+@ContextConfiguration(classes=UserDaoIntegrationTestConfiguration.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserDaoIntegrationTest {
+  private static final int TEST_ID = 7806;
+
+  @Autowired
+  private UserDao userDao;
+
+  @Autowired
+  @Qualifier("usersCache")
+  private Cache cache;
+
+  private JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  private void setDataSource(DataSource ds) {
+    jdbcTemplate = new JdbcTemplate(ds);
+  }
+
+  @Before
+  @After
+  public void fixUser() {
+    jdbcTemplate.update("UPDATE users SET blocked='f' WHERE id=?", TEST_ID);
+    jdbcTemplate.update("DELETE FROM ban_info WHERE userid=?", TEST_ID);
+  }
+
+  @Before
+  public void clearCache() {
+    cache.clear();
+  }
+
+  @Test
+  public void testUser() throws UserNotFoundException {
+    User user = userDao.getUser(TEST_ID);
+
+    assertNotNull(user);
+
+    assertFalse(user.isBlocked());
+  }
+
+  @Test
+  public void testUserCached() throws UserNotFoundException {
+    User user = userDao.getUser(TEST_ID);
+
+    jdbcTemplate.update("UPDATE users SET blocked='t' WHERE id=?", TEST_ID);
+
+    User userCached = userDao.getUserCached(TEST_ID);
+
+    assertFalse(userCached.isBlocked());
+
+    User userNotCached = userDao.getUser(TEST_ID);
+
+    assertTrue(userNotCached.isBlocked());
+  }
+
+  @Test
+  public void testCachePutOnGet() throws UserNotFoundException {
+    cache.evict(TEST_ID);
+
+    User user = userDao.getUser(TEST_ID);
+
+    assertNotNull(user);
+
+    assertFalse(user.isBlocked());
+
+    assertNotNull(cache.get(user.getId()));
+  }
+
+  @Test
+  public void testBlock() throws UserNotFoundException {
+    User user = userDao.getUser(TEST_ID);
+
+    userDao.blockWithoutTransaction(user, user, "");
+
+    User userAfter = userDao.getUser(TEST_ID);
+
+    assertTrue(userAfter.isBlocked());
+  }
+
+  @Test
+  public void testCacheResetOnBlock() throws UserNotFoundException {
+    User user = userDao.getUser(TEST_ID);
+
+    userDao.blockWithoutTransaction(user, user, "");
+
+    User userAfter = userDao.getUserCached(TEST_ID);
+
+    assertTrue(userAfter.isBlocked());
+  }
+}
---
+++ src/test/java/ru/org/linux/user/UserDaoIntegrationTestConfiguration.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 1998-2012 Linux.org.ru
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package ru.org.linux.user;
+
+import com.google.common.collect.ImmutableList;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:database.xml")
+@EnableCaching
+public class UserDaoIntegrationTestConfiguration {
+  private static final String USERS_CACHE = "Users";
+
+  @Bean
+  public UserDao userDao() {
+    return new UserDao();
+  }
+
+  @Bean
+  public IgnoreListDao ignoreListDao() {
+    return new IgnoreListDao();
+  }
+
+  @Bean
+  public CacheManager cacheManager() {
+    SimpleCacheManager cacheManager = new SimpleCacheManager();
+
+    cacheManager.setCaches(
+            ImmutableList.of(new ConcurrentMapCache(USERS_CACHE))
+    );
+
+    return cacheManager;
+  }
+
+  @Bean(name="usersCache")
+  public Cache usersCache(CacheManager cacheManager) {
+    return cacheManager.getCache(USERS_CACHE);
+  }
+
+}
