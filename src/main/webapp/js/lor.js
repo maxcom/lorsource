85b50050c7933e00d9d47f476734710586c6a139
drull755 <drogolov@gmail.com>
2013-01-24 15:10:21 +0200

    Update src/main/webapp/js/lor.js
    disable animation

--- src/main/webapp/js/lor.js
+++ src/main/webapp/js/lor.js
@@ -222,7 +222,7 @@ $(document).ready(function() {
     backButton.text("Вверх");
 
     backButton.click(function() {
-      $("html, body").animate({ scrollTop: 0 });
+      ("html, body").scrollTop(0);
     });
 
     $('#ft').prepend(backButton);
