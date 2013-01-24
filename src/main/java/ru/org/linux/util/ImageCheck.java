5aeae9e0258a599c61d888200d34e2db16e2b32e
Ildar Hizbulin <hizel@vyborg.ru>
2013-01-24 16:39:41 +0400

    ImageCheck: дополнительные проверки для apng

--- src/main/java/ru/org/linux/util/ImageCheck.java
+++ src/main/java/ru/org/linux/util/ImageCheck.java
@@ -14,9 +14,15 @@
  */
 package ru.org.linux.util;
 
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.stream.ImageInputStream;
+import javax.xml.bind.NotIdentifiableEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
@@ -26,7 +32,7 @@ import java.util.Iterator;
 public class ImageCheck {
 
   private final String formatName;
-  private final int numImages;
+  private final boolean animated;
   private final int height;
   private final int width;
 
@@ -37,20 +43,59 @@ public class ImageCheck {
       throw new RuntimeException("No readers");
     }
     ImageReader reader = iter.next();
-    reader.setInput(iis, false, false);
+    reader.setInput(iis);
     formatName = reader.getFormatName();
-    numImages = reader.getNumImages(true);
-    height = reader.getHeight(reader.getMinIndex());
-    width = reader.getWidth(reader.getMinIndex());
+    if("png".equals(formatName) && hasAnimatedPng(reader)) {
+      animated = true;
+    } else {
+      if(reader.getNumImages(true) > 1) {
+        animated = true;
+      } else {
+        animated = false;
+      }
+    }
+    height = reader.getHeight(0);
+    width = reader.getWidth(0);
     iis.close();
+  }
+
+  private boolean hasAnimatedPng(ImageReader reader) throws IOException {
+    if(! "png".equals(reader.getFormatName())) {
+      return false;
+    }
+    IIOMetadata metadata = reader.getImageMetadata(0);
+
+    for(String name : metadata.getMetadataFormatNames()) {
+      Node root = metadata.getAsTree(name);
+      for(int i=0; i < root.getChildNodes().getLength(); i++) {
+        Node child = root.getChildNodes().item(i);
+        if("UnknownChunks".equals(child.getNodeName())) {
+          NodeList unknownChunks = child.getChildNodes();
+          for(int j=0; j < unknownChunks.getLength(); j++) {
+            if("UnknownChunk".equals(unknownChunks.item(j).getNodeName())) {
+              NamedNodeMap map = unknownChunks.item(j).getAttributes();
+              for(int k=0; k<map.getLength(); k++) {
+                Node attr = map.item(k);
+                if("type".equals(attr.getNodeName()) && ("acTL".equals(attr.getNodeValue()) || "fcTL".equals(attr.getNodeValue()))) {
+                  return true;
+                }
+              }
+            }
+          }
+
+        }
+      }
+
+    }
+    return false;
   }
 
   public String getFormatName() {
     return formatName;
   }
 
-  public int getNumImages() {
-    return numImages;
+  public boolean isAnimated() {
+    return animated;
   }
 
   public int getHeight() {
--- src/test/java/ru/org/linux/util/ImageCheckTest.java
+++ src/test/java/ru/org/linux/util/ImageCheckTest.java
@@ -26,14 +26,14 @@ public class ImageCheckTest {
   @Test
   public void gifTest() throws Exception {
     ImageCheck typicalGif = new ImageCheck(new File("src/test/resources/images/typical-gif.gif"));
-    Assert.assertEquals(1, typicalGif.getNumImages());
+    Assert.assertFalse(typicalGif.isAnimated());
     Assert.assertEquals("gif", typicalGif.getFormatName());
     Assert.assertEquals(9, typicalGif.getHeight());
     Assert.assertEquals(9, typicalGif.getWidth());
 
 
     ImageCheck failExtGif = new ImageCheck(new File("src/test/resources/images/failext-gif.png"));
-    Assert.assertEquals(1, failExtGif.getNumImages());
+    Assert.assertFalse(failExtGif.isAnimated());
     Assert.assertEquals("gif", failExtGif.getFormatName());
     Assert.assertEquals(9, failExtGif.getHeight());
     Assert.assertEquals(9, failExtGif.getWidth());
@@ -42,14 +42,14 @@ public class ImageCheckTest {
   @Test
   public void pngTest() throws Exception {
     ImageCheck typical = new ImageCheck(new File("src/test/resources/images/typical-png.png"));
-    Assert.assertEquals(1, typical.getNumImages());
+    Assert.assertFalse(typical.isAnimated());
     Assert.assertEquals("png", typical.getFormatName());
     Assert.assertEquals(32, typical.getHeight());
     Assert.assertEquals(32, typical.getWidth());
 
 
     ImageCheck failExt = new ImageCheck(new File("src/test/resources/images/failext-png.jpg"));
-    Assert.assertEquals(1, failExt.getNumImages());
+    Assert.assertFalse(failExt.isAnimated());
     Assert.assertEquals("png", failExt.getFormatName());
     Assert.assertEquals(32, failExt.getHeight());
     Assert.assertEquals(32, failExt.getWidth());
@@ -58,14 +58,14 @@ public class ImageCheckTest {
   @Test
   public void jpgTest() throws Exception {
     ImageCheck typical = new ImageCheck(new File("src/test/resources/images/typical-jpg.jpg"));
-    Assert.assertEquals(1, typical.getNumImages());
+    Assert.assertFalse(typical.isAnimated());
     Assert.assertEquals("JPEG", typical.getFormatName());
     Assert.assertEquals(400, typical.getHeight());
     Assert.assertEquals(240, typical.getWidth());
 
 
     ImageCheck failExt = new ImageCheck(new File("src/test/resources/images/failext-jpg.png"));
-    Assert.assertEquals(1, failExt.getNumImages());
+    Assert.assertFalse(failExt.isAnimated());
     Assert.assertEquals("JPEG", failExt.getFormatName());
     Assert.assertEquals(400, failExt.getHeight());
     Assert.assertEquals(240, failExt.getWidth());
@@ -73,7 +73,34 @@ public class ImageCheckTest {
 
   @Test
   public void crapTest() throws Exception {
-    ImageCheck crap = new ImageCheck(new File("src/test/resources/images/crap.png"));
+    boolean r = false;
+    try {
+      ImageCheck crap = new ImageCheck(new File("src/test/resources/images/crap.png"));
+    } catch (Exception e) {
+      r = true;
+    }
+    Assert.assertTrue(r);
+
+  }
+
+  @Test
+  public void animatedGifTest() throws Exception {
+    ImageCheck anima1 = new ImageCheck(new File("src/test/resources/images/animated-gif.gif"));
+    ImageCheck anima2 = new ImageCheck(new File("src/test/resources/images/animated-gif.png"));
+    ImageCheck anima3 = new ImageCheck(new File("src/test/resources/images/animated-gif.jpg"));
+    Assert.assertTrue(anima1.isAnimated());
+    Assert.assertTrue(anima2.isAnimated());
+    Assert.assertTrue(anima3.isAnimated());
+  }
+
+  @Test
+  public void animatedPngTest() throws Exception {
+    ImageCheck anima1 = new ImageCheck(new File("src/test/resources/images/animated-png.gif"));
+    ImageCheck anima2 = new ImageCheck(new File("src/test/resources/images/animated-png.png"));
+    ImageCheck anima3 = new ImageCheck(new File("src/test/resources/images/animated-png.jpg"));
+    Assert.assertTrue(anima1.isAnimated());
+    Assert.assertTrue(anima2.isAnimated());
+    Assert.assertTrue(anima3.isAnimated());
   }
 
 }
