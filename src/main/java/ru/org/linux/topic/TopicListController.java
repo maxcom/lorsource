8c4c6a0bd5a34ea9d8ede1c81dcd053c9ee42208
Maxim Valyanskiy <max.valjanski@gmail.com>
2013-03-23 14:49:34 +0400

    TopicListContoller cleanup

--- src/main/java/ru/org/linux/topic/TopicListController.java
+++ src/main/java/ru/org/linux/topic/TopicListController.java
@@ -35,7 +35,9 @@ import ru.org.linux.section.SectionService;
 import ru.org.linux.site.ScriptErrorException;
 import ru.org.linux.site.Template;
 import ru.org.linux.tag.TagService;
-import ru.org.linux.user.*;
+import ru.org.linux.user.UserErrorException;
+import ru.org.linux.user.UserNotFoundException;
+import ru.org.linux.user.UserTagService;
 import ru.org.linux.util.*;
 
 import javax.annotation.Nullable;
@@ -66,10 +68,6 @@ public class TopicListController {
   // TODO: здесь должен быть сервис, а не DAO
   @Autowired
   private GroupDao groupDao;
-
-  // TODO: здесь должен быть сервис, а не DAO
-  @Autowired
-  private UserDao userDao;
 
   @RequestMapping(value = "/view-news.jsp", method = {RequestMethod.GET, RequestMethod.HEAD}, params = {"tag"})
   public View tagFeedOld(
@@ -207,7 +205,7 @@ public class TopicListController {
       }
     }
 
-    setExpireHeaders(response, topicListForm);
+    setExpireHeaders(response, topicListForm.getYear(), topicListForm.getMonth());
 
     modelAndView.addObject("navtitle", calculateNavTitle(section, group, topicListForm));
 
@@ -431,162 +429,6 @@ public class TopicListController {
 
   /**
    * @param nick
-   * @param request
-   * @param response
-   * @return
-   * @throws Exception
-   */
-  @RequestMapping("/people/{nick}")
-  public ModelAndView showUserTopicsNew(
-    HttpServletRequest request,
-    TopicListRequest topicListForm,
-    @PathVariable String nick,
-    HttpServletResponse response
-  ) throws Exception {
-    setExpireHeaders(response, topicListForm);
-
-    ModelAndView modelAndView = new ModelAndView();
-
-    Section section = null;
-    if (topicListForm.getSection() != null && topicListForm.getSection() != 0) {
-      section = sectionService.getSection(topicListForm.getSection());
-    }
-
-    if (topicListForm.getTag() != null) {
-      tagService.checkTag(topicListForm.getTag());
-    }
-
-    User user = getUserByNickname(modelAndView, nick);
-
-    UserInfo userInfo = userDao.getUserInfoClass(user);
-    modelAndView.addObject("meLink", userInfo.getUrl());
-
-    modelAndView.addObject("url", "/people/" + nick + '/');
-    modelAndView.addObject("whoisLink", "/people/" + nick + '/' + "profile");
-    // TODO: modelAndView.addObject("archiveLink", "/people/"+nick+"/archive/");
-
-
-    modelAndView.addObject("ptitle", "Сообщения " + user.getNick());
-    modelAndView.addObject("navtitle", "Сообщения " + user.getNick());
-
-    modelAndView.addObject("rssLink", "/people/" + nick + "/?output=rss");
-
-    topicListForm.setOffset(
-      topicListService.fixOffset(topicListForm.getOffset())
-    );
-    modelAndView.addObject("offsetNavigation", true);
-    modelAndView.addObject("topicListForm", topicListForm);
-
-    List<Topic> messages = topicListService.getUserTopicsFeed(
-      user,
-      section,
-      null,
-      topicListForm.getOffset(),
-      false,
-      false
-    );
-
-    boolean rss = topicListForm.getOutput() != null && "rss".equals(topicListForm.getOutput());
-    if (!rss) {
-      if (section != null) {
-        modelAndView.addObject("section", section);
-      }
-      modelAndView.addObject("sectionList", sectionService.getSectionList());
-    }
-
-    if (Integer.valueOf(0).equals(topicListForm.getSection())) {
-      topicListForm.setSection(null);
-    }
-    URLUtil.QueryString queryString = new URLUtil.QueryString();
-    queryString.add("section", topicListForm.getSection());
-    queryString.add("tag", topicListForm.getTag());
-    modelAndView.addObject("params", queryString.toString());
-
-    prepareTopicsForPlainOrRss(request, modelAndView, topicListForm, messages);
-
-    if (!rss) {
-      modelAndView.setViewName("user-topics");
-    }
-
-    return modelAndView;
-  }
-
-  /**
-   * @param nick
-   * @param topicListForm
-   * @param request
-   * @param response
-   * @return
-   * @throws Exception
-   */
-  @RequestMapping(value="/people/{nick}/favs", params="!output")
-  public ModelAndView showUserFavs(
-    HttpServletRequest request,
-    TopicListRequest topicListForm,
-    @PathVariable String nick,
-    HttpServletResponse response
-  ) throws Exception {
-    setExpireHeaders(response, topicListForm);
-
-    ModelAndView modelAndView = new ModelAndView();
-
-    User user = getUserByNickname(modelAndView, nick);
-
-    modelAndView.addObject("url", "/people/" + nick + "/favs");
-    modelAndView.addObject("whoisLink", "/people/" + nick + '/' + "profile");
-
-    modelAndView.addObject("ptitle", "Избранные сообщения " + user.getNick());
-    modelAndView.addObject("navtitle", "Избранные сообщения " + user.getNick());
-
-    topicListForm.setOffset(
-      topicListService.fixOffset(topicListForm.getOffset())
-    );
-    modelAndView.addObject("offsetNavigation", true);
-    modelAndView.addObject("topicListForm", topicListForm);
-
-    List<Topic> messages = topicListService.getUserTopicsFeed(user, topicListForm.getOffset(), true, false);
-    prepareTopicsForPlainOrRss(request, modelAndView, topicListForm, messages);
-
-    modelAndView.setViewName("user-topics");
-
-    return modelAndView;
-  }
-
-  @RequestMapping(value = "/people/{nick}/tracked", params="!output")
-  public ModelAndView showUserWatches(
-    HttpServletRequest request,
-    TopicListRequest topicListForm,
-    @PathVariable String nick,
-    HttpServletResponse response
-  ) throws Exception {
-    setExpireHeaders(response, topicListForm);
-
-    ModelAndView modelAndView = new ModelAndView();
-
-    User user = getUserByNickname(modelAndView, nick);
-
-    modelAndView.addObject("url", "/people/" + nick + "/tracked");
-    modelAndView.addObject("whoisLink", "/people/" + nick + '/' + "profile");
-
-    modelAndView.addObject("ptitle", "Отслеживаемые сообщения " + user.getNick());
-    modelAndView.addObject("navtitle", "Отслеживаемые сообщения " + user.getNick());
-
-    topicListForm.setOffset(
-      topicListService.fixOffset(topicListForm.getOffset())
-    );
-    modelAndView.addObject("offsetNavigation", true);
-    modelAndView.addObject("topicListForm", topicListForm);
-
-    List<Topic> messages = topicListService.getUserTopicsFeed(user, topicListForm.getOffset(), true, true);
-    prepareTopicsForPlainOrRss(request, modelAndView, topicListForm, messages);
-
-    modelAndView.setViewName("user-topics");
-
-    return modelAndView;
-  }
-
-  /**
-   * @param nick
    * @param output
    * @return
    */
@@ -748,60 +590,6 @@ public class TopicListController {
     return modelAndView;
   }
 
-
-  /**
-   * @param request
-   * @param modelAndView
-   * @param topicListForm
-   * @param messages
-   */
-  private void prepareTopicsForPlainOrRss(
-    HttpServletRequest request,
-    ModelAndView modelAndView,
-    TopicListRequest topicListForm,
-    List<Topic> messages
-  ) {
-    boolean rss = topicListForm.getOutput() != null && "rss".equals(topicListForm.getOutput());
-    if (rss) {
-      modelAndView.addObject(
-        "messages",
-        prepareService.prepareMessages(messages, request.isSecure())
-      );
-      modelAndView.setViewName("section-rss");
-    } else {
-      Template tmpl = Template.getTemplate(request);
-      modelAndView.addObject(
-        "messages",
-        prepareService.prepareMessagesForUser(
-                messages,
-                request.isSecure(),
-                tmpl.getCurrentUser(),
-                tmpl.getProf(),
-                false
-        )
-      );
-
-      modelAndView.setViewName("view-news");
-    }
-  }
-
-  /**
-   * @param modelAndView
-   * @param nick
-   * @return
-   * @throws UserNotFoundException
-   * @throws UserErrorException
-   */
-  private User getUserByNickname(ModelAndView modelAndView, String nick)
-    throws UserNotFoundException, UserErrorException {
-    User user = userDao.getUser(nick);
-    if (user.getId() == User.ANONYMOUS_ID) {
-      throw new UserErrorException("Лента для пользователя anonymous не доступна");
-    }
-    modelAndView.addObject("user", user);
-    return user;
-  }
-
   private ModelAndView group(
     HttpServletRequest request,
     TopicListRequest topicListForm,
@@ -833,15 +621,11 @@ public class TopicListController {
     return modelAndView;
   }
 
-  /**
-   * @param response
-   * @param topicListForm
-   */
-  private static void setExpireHeaders(
+  public static void setExpireHeaders(
           HttpServletResponse response,
-          TopicListRequest topicListForm
+          Integer year, Integer month
   ) {
-    if (topicListForm.getMonth() == null) {
+    if (month == null) {
       response.setDateHeader("Expires", System.currentTimeMillis() + 60 * 1000);
       response.setDateHeader("Last-Modified", System.currentTimeMillis());
     } else {
@@ -849,8 +633,8 @@ public class TopicListController {
 
       Calendar calendar = Calendar.getInstance();
       calendar.set(
-        topicListForm.getYear(),
-        topicListForm.getMonth() - 1, 1
+        year,
+        month - 1, 1
       );
       calendar.add(Calendar.MONTH, 1);
 
@@ -864,7 +648,6 @@ public class TopicListController {
         response.setDateHeader("Last-Modified", System.currentTimeMillis());
       }
     }
-
   }
 
   /**
