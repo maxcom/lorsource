5f81a81d57cd429e165b8cc5112a532f4a6b845a
Maxim Valyanskiy <max.valjanski@gmail.com>
2013-03-26 21:44:58 +0400

    пара базовых web-тестов

---
+++ src/test/java/ru/org/linux/spring/MainPageWebTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 1998-2012 Linux.org.ru
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package ru.org.linux.spring;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import org.apache.commons.httpclient.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import ru.org.linux.test.WebHelper;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ */
+public class MainPageWebTest {
+  private WebResource resource;
+
+  @Before
+  public void initResource() throws Exception {
+    Client client = new Client();
+    client.setFollowRedirects(false);
+    resource = client.resource(WebHelper.MAIN_URL);
+  }
+
+  @Test
+  public void testMain() throws IOException {
+    ClientResponse cr = resource
+        .path("/")
+        .get(ClientResponse.class);
+    assertEquals(HttpStatus.SC_OK, cr.getStatus());
+  }
+}
