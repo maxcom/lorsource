6801c0a5d0c9e36527c53a0a04fd69b1438a0a96
Maxim Valyanskiy <max.valjanski@gmail.com>
2013-11-10 14:24:30 +0400

    SectionController: error404 если раздел не найден
    вместо error 500

--- src/main/java/ru/org/linux/section/SectionController.java
+++ src/main/java/ru/org/linux/section/SectionController.java
@@ -16,9 +16,12 @@
 package ru.org.linux.section;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
@@ -55,5 +58,11 @@ public class SectionController {
   @RequestMapping(value="/view-section.jsp", params = {"section=2"})
   public View forumOld() {
     return new RedirectView("/forum/");
+  }
+
+  @ExceptionHandler(SectionNotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public ModelAndView handleNotFoundException() {
+    return new ModelAndView("errors/code404");
   }
 }
