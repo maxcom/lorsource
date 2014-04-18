f07edf957e9a7a1a725d56a20e1efeb8100ebd39
Maxim Valyanskiy <max.valjanski@gmail.com>
2014-04-18 22:28:14 +0400

    hsts для одной звезды

--- src/main/scala/ru/org/linux/auth/HSTSInterceptor.scala
+++ src/main/scala/ru/org/linux/auth/HSTSInterceptor.scala
@@ -6,11 +6,11 @@ import ru.org.linux.spring.SiteConfig
 import org.springframework.beans.factory.annotation.Autowired
 
 class HstsInterceptor @Autowired() (config:SiteConfig) extends HandlerInterceptorAdapter {
-  override def preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: scala.Any): Boolean = {
+  override def preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any) = {
     if (request.isSecure && config.enableHsts()) {
       val currentUser = AuthUtil.getCurrentUser
 
-      if (currentUser!=null && currentUser.getMaxScore>=200) {
+      if (currentUser!=null && currentUser.getMaxScore>=100) {
         response.addHeader("Strict-Transport-Security", "max-age=7776000")
       }
     }
