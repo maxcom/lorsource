bbc578b84bba2db86d45d6a55f73f81559961f00
Andrej A Antonov <polymorphm@gmail.com>
2014-07-28 03:38:42 +0400

    changed 'ru.org.linux.clickjacking.CJInterceptor' to 'ru.org.linux.csp.CSPInterceptor'

--- src/main/scala/ru/org/linux/csp/CSPInterceptor.scala
+++ src/main/scala/ru/org/linux/csp/CSPInterceptor.scala
@@ -1,18 +1,19 @@
-package ru.org.linux.clickjacking
+package ru.org.linux.csp
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 import ru.org.linux.spring.SiteConfig
 import org.springframework.beans.factory.annotation.Autowired
 
-class CJInterceptor @Autowired() (config:SiteConfig) extends HandlerInterceptorAdapter {
+class CSPInterceptor @Autowired() (config:SiteConfig) extends HandlerInterceptorAdapter {
   override def preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any) = {
     //if (!config.blahblahblahblah()) {
     //  return true
     //}
     
-    response.addHeader("X-Frame-Options", "DENY")
-    // или же быть может (вместо "DENY") лучше использовать "SAMEORIGIN"?
+    response.addHeader("Content-Security-Policy", "frame-options 'deny'")
+    // вообще-то правила можно было бы придумать и более гибкие чем эти.
+    // но начала -- пойдёт! лучше чем ни чего :-)
 
     true
   }
