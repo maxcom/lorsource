2bf4cf1c503826fc475e5f3b5df3ef505b9955a9
Maxim Valyanskiy <max.valjanski@gmail.com>
2014-11-06 22:00:59 +0300

    remove solarized swamp theme

--- src/main/java/ru/org/linux/site/Theme.java
+++ src/main/java/ru/org/linux/site/Theme.java
@@ -22,7 +22,6 @@ public class Theme {
   private static final Theme WHITE2 = new Theme("white2", "white2/head-main.jsp", "white2/head-main.jsp");
   private static final Theme TANGO = new Theme("tango", "tango/head-main.jsp", "tango/head-main.jsp");
   private static final Theme WALTZ = new Theme("waltz", "tango/head-main.jsp", "tango/head-main.jsp");
-  private static final Theme SOLARIZED_SWAMP = new Theme("solarized-swamp", "tango/head-main.jsp", "tango/head-main.jsp", true);
   private static final Theme ZOMG_PONIES = new Theme("zomg_ponies", "zomg_ponies/head-main.jsp", "zomg_ponies/head-main.jsp");
 
   public static final ImmutableList<Theme> THEMES = ImmutableList.of(
@@ -30,8 +29,7 @@ public class Theme {
           BLACK,
           WHITE2,
           WALTZ,
-          SOLARIZED_SWAMP,
-	  ZOMG_PONIES
+	      ZOMG_PONIES
   );
 
   private final String id;
