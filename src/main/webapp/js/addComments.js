16ca77f95ed26694774db6181913035e311aea52
Maxim Valyanskiy <max.valjanski@gmail.com>
2014-11-25 15:28:14 +0300

    поправил загрузку addComment.js

--- src/main/webapp/js/addComments.js
+++ src/main/webapp/js/addComments.js
@@ -13,83 +13,86 @@
  *    limitations under the License.
  */
 
-var element = '';
-var csrf = '';
+$script.ready('jquery', function() {
+  var element = '';
+  var csrf = '';
 
-function sh(type, id) {
-  if (csrf.length>0) {
-    $("input[name='csrf']").attr('value', csrf);
+  function sh(type, id) {
+    if (csrf.length>0) {
+      $("input[name='csrf']").attr('value', csrf);
+    }
+
+    if (type == 1) {
+      reply_to = $("input[name='replyto']", element);
+      if (reply_to.attr('value') != id) {
+        element.hide();
+      }
+
+      if (element.is(':hidden')) {
+        reply = $('div.reply', $('div.msg_body', $('#comment-' + id)));
+        reply.append(element);
+        reply_to.attr('value', id);
+        element.slideDown('slow', function() { $("#msg").focus(); });
+      } else {
+        element.slideUp('slow');
+      }
+    } else if (type == 0) {
+      topic_id = $("input[name='topic']", element).attr('value');
+
+      reply_to = $("input[name='replyto']", element);
+      if (reply_to.attr('value') != 0) {
+        element.hide();
+      }
+
+      if (element.is(':hidden')) {
+        reply = $('div.reply', $('div.msg_body', $('#topic-' + topic_id)));
+        reply.append(element);
+        reply_to.attr('value', '0');
+        element.slideDown('slow', function() { $("#msg").focus(); });
+      } else {
+        element.slideUp('slow');
+      }
+    }
   }
 
-  if (type == 1) {
-    reply_to = $("input[name='replyto']", element);
-    if (reply_to.attr('value') != id) {
-      element.hide();
+  $(document).ready(function() {
+    element = $("#commentForm").parent();
+
+    if (document.cookie.match(/CSRF_TOKEN\=(\w+)\;?/)) {
+      csrf = document.cookie.match(/CSRF_TOKEN\=(\w+)\;?/);
+      csrf = csrf[1];
     }
 
-    if (element.is(':hidden')) {
-      reply = $('div.reply', $('div.msg_body', $('#comment-' + id)));
-      reply.append(element);
-      reply_to.attr('value', id);
-      element.slideDown('slow', function() { $("#msg").focus(); });
-    } else {
-      element.slideUp('slow');
-    }
-  } else if (type == 0) {
-    topic_id = $("input[name='topic']", element).attr('value');
-
-    reply_to = $("input[name='replyto']", element);
-    if (reply_to.attr('value') != 0) {
-      element.hide();
-    }
-
-    if (element.is(':hidden')) {
-      reply = $('div.reply', $('div.msg_body', $('#topic-' + topic_id)));
-      reply.append(element);
-      reply_to.attr('value', '0');
-      element.slideDown('slow', function() { $("#msg").focus(); });
-    } else {
-      element.slideUp('slow');
-    }
-  }
-}
-
-$(document).ready(function() {
-  element = $("#commentForm").parent();
-
-  if (document.cookie.match(/CSRF_TOKEN\=(\w+)\;?/)) {
-    csrf = document.cookie.match(/CSRF_TOKEN\=(\w+)\;?/);
-    csrf = csrf[1];
-  }
-
-  $('div.reply').each(function() {
-    $('a[href^="comment-message.jsp"]', this).bind("click", function() {
-      sh(0, 0);
-      return false;
-    });
-
-    var lnk = $('a[href^="add_comment.jsp"]', this);
-    if (lnk.length>0) {
-      var buff = lnk.attr('href').match(/\d+/g);
-      var idr = buff[1];
-      lnk.bind("click", function() {
-        sh(1, idr);
+    $('div.reply').each(function() {
+      $('a[href^="comment-message.jsp"]', this).bind("click", function() {
+        sh(0, 0);
         return false;
       });
-    }
+
+      var lnk = $('a[href^="add_comment.jsp"]', this);
+      if (lnk.length>0) {
+        var buff = lnk.attr('href').match(/\d+/g);
+        var idr = buff[1];
+        lnk.bind("click", function() {
+          sh(1, idr);
+          return false;
+        });
+      }
+    });
+
+    window.onbeforeunload = function() {
+      if ($("#msg").val()!='' && ! $("#commentForm").parent().is(":hidden")) {
+        return "Вы что-то напечатали в форме. Все введенные данные будут потеряны при закрытии страницы.";
+      }
+    };
+
+    $("#commentForm").bind("submit", function() {
+      window.onbeforeunload = null;
+    });
+
+    $("#commentForm").bind("reset", function() {
+      element.slideUp('slow');
+    })
   });
-
-  window.onbeforeunload = function() {
-    if ($("#msg").val()!='' && ! $("#commentForm").parent().is(":hidden")) {
-      return "Вы что-то напечатали в форме. Все введенные данные будут потеряны при закрытии страницы.";
-    }
-  };
-
-  $("#commentForm").bind("submit", function() {
-    window.onbeforeunload = null;
-  });
-
-  $("#commentForm").bind("reset", function() {
-    element.slideUp('slow');
-  })
 });
+
