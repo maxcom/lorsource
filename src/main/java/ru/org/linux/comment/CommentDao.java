46f0c68b3f5f8529438829cc171fff15d82466d3
Maxim Valyanskiy <max.valjanski@gmail.com>
2015-03-03 09:19:07 +0300

    удалил неиспользуемую колонку topics.stat2

--- src/main/java/ru/org/linux/comment/CommentDao.java
+++ src/main/java/ru/org/linux/comment/CommentDao.java
@@ -141,7 +141,6 @@ public class CommentDao {
     int topicId = jdbcTemplate.queryForObject("SELECT topic FROM comments WHERE id=?", Integer.class, commentId);
 
     jdbcTemplate.update("UPDATE topics SET stat1=stat1-?, lastmod=CURRENT_TIMESTAMP WHERE id = ?", count, topicId);
-    jdbcTemplate.update("UPDATE topics SET stat2=stat1 WHERE id=? AND stat2 > stat1", topicId);
     jdbcTemplate.update("UPDATE topics SET stat3=stat1 WHERE id=? AND stat3 > stat1", topicId);
   }
 
