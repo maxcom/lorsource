24f6298f977748d7b988921f78d14ba73de1f492
Maxim Valyanskiy <max.valjanski@gmail.com>
2015-05-19 21:36:50 +0300

    remove usage of deprecated PasswordEncoder

--- src/main/java/ru/org/linux/auth/PasswordEncoderImpl.java
+++ src/main/java/ru/org/linux/auth/PasswordEncoderImpl.java
@@ -17,19 +17,20 @@ package ru.org.linux.auth;
 
 import org.jasypt.util.password.BasicPasswordEncryptor;
 import org.jasypt.util.password.PasswordEncryptor;
-import org.springframework.security.authentication.encoding.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordEncoderImpl implements PasswordEncoder {
-  static PasswordEncryptor encryptor = new BasicPasswordEncryptor();
+  private static final PasswordEncryptor encryptor = new BasicPasswordEncryptor();
 
-  public String encodePassword(String rawPass, Object salt) {
-    return encryptor.encryptPassword(rawPass);
+  @Override
+  public String encode(CharSequence rawPassword) {
+    return encryptor.encryptPassword(rawPassword.toString());
   }
 
-  public boolean isPasswordValid(String encPass, String rawPass, Object salt) {
-    return encryptor.checkPassword(rawPass, encPass);
+  @Override
+  public boolean matches(CharSequence rawPassword, String encodedPassword) {
+    return encryptor.checkPassword(rawPassword.toString(), encodedPassword);
   }
-
 }
