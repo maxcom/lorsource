d0e413fad0cc02e4a14332768258253e1605b135
Maxim Valyanskiy <max.valjanski@gmail.com>
2015-06-11 09:03:15 +0300

    score не начисляется за S&E по аналогии с Talks

--- src/main/java/ru/org/linux/user/ScoreUpdater.java
+++ src/main/java/ru/org/linux/user/ScoreUpdater.java
@@ -46,7 +46,7 @@ public class ScoreUpdater {
             "(select distinct comments.userid from comments, topics " +
             "where comments.postdate>CURRENT_TIMESTAMP-'2 days'::interval " +
             "and topics.id=comments.topic and " +
-            "groupid!=8404 and groupid!=4068 and groupid!=19390 and " +
+            "groupid!=8404 and groupid!=4068 and groupid!=19390 and groupid!=19405score and " +
             "not comments.deleted and not topics.deleted)");
 
     updateMaxScore();
