d00989809858366cc2e54f1cfdf41ca3cf415164
Maxim Valyanskiy <max.valjanski@gmail.com>
2015-07-01 09:17:14 +0300

    elastic4s 1.6.2

--- src/main/scala/ru/org/linux/search/SearchViewer.scala
+++ src/main/scala/ru/org/linux/search/SearchViewer.scala
@@ -90,7 +90,7 @@ class SearchViewer(query:SearchRequest, javaElastic: Client) {
       search in MessageIndexTypes fields (
           Fields: _*
         ) query esQuery sort (
-          sortby field query.getSort.getColumn order query.getSort.order
+          field sort query.getSort.getColumn order query.getSort.order
         ) aggs(
           agg filter "sections" filter matchAllFilter aggs (
             agg terms "sections" field "section" size 0 aggs (
