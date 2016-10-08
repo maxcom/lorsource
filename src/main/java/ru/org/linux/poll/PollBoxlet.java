cea63b6fc828bf22590b6d9b027505ae5e9566d0
kloun <andrey0bolkonsky@gmail.com>
2016-10-08 15:23:33 +0300

    fixed build

--- src/main/java/ru/org/linux/poll/PollBoxlet.java
+++ src/main/java/ru/org/linux/poll/PollBoxlet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2015 Linux.org.ru
+ * Copyright 1998-2016 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -19,7 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import ru.org.linux.spring.boxlets.AbstractBoxlet;
+import ru.org.linux.boxlets.AbstractBoxlet;
 import ru.org.linux.topic.Topic;
 import ru.org.linux.topic.TopicDao;
 
