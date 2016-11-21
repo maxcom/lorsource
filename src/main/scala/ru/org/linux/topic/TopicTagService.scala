fe46b5bb1be4eca6c79cba43e8b7fd0d856fc72c
Maxim Valyanskiy <max.valjanski@gmail.com>
2016-11-21 22:20:29 +0300

    TopicTagService: cleanup

--- src/main/scala/ru/org/linux/topic/TopicTagService.scala
+++ src/main/scala/ru/org/linux/topic/TopicTagService.scala
@@ -24,14 +24,15 @@ import ru.org.linux.tag.TagService._
 import ru.org.linux.tag.{TagRef, _}
 import ru.org.linux.topic.TopicTagService._
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+import scala.collection.Map
 
 @Service
 class TopicTagService(val transactionManager: PlatformTransactionManager, tagService: TagModificationService,
                       topicTagDao: TopicTagDao) extends StrictLogging with TransactionManagement {
 
   tagService.getActionHandlers.add(new ITagActionHandler() {
-    override def replaceTag(oldTagId: Int, newTagId: Int, newTagName: String):Unit = {
+    override def replaceTag(oldTagId: Int, newTagId: Int, newTagName: String): Unit = {
       val tagCount = topicTagDao.getCountReplacedTags(oldTagId, newTagId)
       topicTagDao.replaceTag(oldTagId, newTagId)
       topicTagDao.increaseCounterById(newTagId, tagCount)
@@ -39,15 +40,13 @@ class TopicTagService(val transactionManager: PlatformTransactionManager, tagSer
       logger.debug(s"Счётчик использование тега '$newTagName' увеличен на $tagCount")
     }
 
-    override def deleteTag(tagId: Int, tagName: String):Unit = {
+    override def deleteTag(tagId: Int, tagName: String): Unit = {
       topicTagDao.deleteTag(tagId)
       logger.debug("Удалено использование тега '{}' в топиках", tagName)
     }
   })
 
-  def reCalculateAllCounters():Unit = {
-    topicTagDao.reCalculateAllCounters()
-  }
+  def reCalculateAllCounters(): Unit = topicTagDao.reCalculateAllCounters()
 
   /**
    * Обновить список тегов сообщения по идентификационному номеру сообщения.
@@ -56,70 +55,50 @@ class TopicTagService(val transactionManager: PlatformTransactionManager, tagSer
    * @param tagList новый список тегов.
    * @return true если были произведены изменения
    */
-  def updateTags(msgId:Int, oldTags:java.util.List[String], tagList:java.util.List[String]):Boolean = {
+  def updateTags(msgId: Int, oldTags: java.util.List[String], tagList: java.util.List[String]): Boolean = {
     transactional() { _ =>
-      logger.debug("Обновление списка тегов [" + tagList.toString + "] для топика msgId=" + msgId)
+      logger.debug(s"Обновление списка тегов [${tagList.toString}] для топика msgId=$msgId")
 
       val oldTags = getTags(msgId)
 
-      val newTags = tagList.filter(!oldTags.contains(_))
+      val newTags = tagList.asScala.filterNot(oldTags.contains)
 
       for (tag <- newTags) {
         val id = tagService.getOrCreateTag(tag)
 
-        logger.trace("Добавлен тег '" + tag + "' к топику msgId=" + msgId)
+        logger.trace(s"Добавлен тег '$tag' к топику msgId=$msgId")
         topicTagDao.addTag(msgId, id)
       }
 
-      val deleteTags = oldTags.filter(!tagList.contains(_))
+      val deleteTags = oldTags.asScala.filterNot(tagList.contains)
 
       for (tag <- deleteTags) {
         val id = tagService.getOrCreateTag(tag)
-        logger.trace("Удалён тег '" + tag + "' к топику msgId=" + msgId)
+        logger.trace(s"Удалён тег '$tag' к топику msgId=$msgId")
         topicTagDao.deleteTag(msgId, id)
       }
 
-      logger.trace("Завершено: обновление списка тегов для топика msgId=" + msgId)
+      logger.trace(s"Завершено: обновление списка тегов для топика msgId=$msgId")
 
-      val modified = newTags.nonEmpty || deleteTags.nonEmpty
-
-      if (modified) {
-        updateCounters(oldTags, newTags)
-      }
-
-      modified
+      newTags.nonEmpty || deleteTags.nonEmpty
     }
-  }
-
-  /**
-   * Обновить счётчики по тегам.
-   *
-   * @param oldTags список старых тегов
-   * @param newTags список новых тегов
-   */
-  private[this] def updateCounters(oldTags:Seq[String], newTags:Seq[String]):Unit = {
-    logger.debug(
-            "Обновление счётчиков тегов; старые теги [{}]; новые теги [{}]",
-            oldTags,
-            newTags
-    )
   }
 
   /**
    * Получить все теги сообщения по идентификационному номеру сообщения.
    *
    */
-  def getTags(topic:Topic):java.util.List[String] = topicTagDao.getTags(topic.getId).map(_.name)
+  def getTags(topic:Topic): java.util.List[String] = topicTagDao.getTags(topic.getId).map(_.name).asJava
 
-  private def getTags(msgId:Int):java.util.List[String] = topicTagDao.getTags(msgId).map(_.name)
+  private def getTags(msgId:Int): java.util.List[String] = topicTagDao.getTags(msgId).map(_.name).asJava
 
-  def getTagRefs(topic:Topic):java.util.List[TagRef] =
-    topicTagDao.getTags(topic.getId).map(tag => tagRef(tag))
+  def getTagRefs(topic: Topic): java.util.List[TagRef] =
+    topicTagDao.getTags(topic.getId).map(tagRef).asJava
 
-  def getTagRefs(topics:java.util.List[Topic]):ImmutableListMultimap[Integer, TagRef] = {
+  def getTagRefs(topics:java.util.List[Topic]): ImmutableListMultimap[Integer, TagRef] = {
     val builder = ImmutableListMultimap.builder[Integer,TagRef]()
 
-    val tags = topicTagDao.getTags(topics.map(_.getId))
+    val tags = topicTagDao.getTags(topics.asScala.map(_.getId))
 
     for ((msgid, tag) <- tags) {
       builder.put(msgid, tagRef(tag))
@@ -128,7 +107,8 @@ class TopicTagService(val transactionManager: PlatformTransactionManager, tagSer
     builder.build()
   }
 
-  def tagRefs(topics:Seq[Int]) = topicTagDao.getTags(topics).groupBy(_._1).mapValues(_.map(_._2))
+  def tagRefs(topics: Seq[Int]): Map[Int, Seq[TagInfo]] =
+    topicTagDao.getTags(topics).groupBy(_._1).mapValues(_.map(_._2))
 
   /**
    * Получить все теги сообщения по идентификационному номеру сообщения.
@@ -139,7 +119,7 @@ class TopicTagService(val transactionManager: PlatformTransactionManager, tagSer
    */
   def getTagsForTitle(msgId:Int): ImmutableList[String] = {
     val tags = topicTagDao.getTags(msgId).map(_.name).take(MaxTagsInTitle)
-    ImmutableList.copyOf(tags.toIterable)
+    ImmutableList.copyOf(tags.toIterable.asJava)
   }
 }
 
