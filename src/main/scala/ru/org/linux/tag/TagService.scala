6cbb3add3fb3934c6c42ea632b78a274c58ba48e
Maxim Valyanskiy <max.valjanski@gmail.com>
2017-01-14 12:08:51 +0300

    "активные теги" считаем с учетом комментариев

--- src/main/scala/ru/org/linux/tag/TagService.scala
+++ src/main/scala/ru/org/linux/tag/TagService.scala
@@ -101,7 +101,6 @@ class TagService(tagDao: TagDao, elastic: ElasticClient) {
         search(MessageIndexTypes) size 0 query
           new BoolQueryDefinition()
             .filter(
-              termQuery("is_comment", "false"),
               termQuery("section", section.getUrlName),
               rangeQuery("postdate").gte("now/d-1y")
             ) aggs (sigTermsAggregation("active") field "tag" minDocCount 10)
