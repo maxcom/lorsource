e950a6da29917e18700bb2256a877cda2bc812cf
Vyacheslav Boyko <mail4bvn@gmail.com>
2018-11-20 14:16:40 +0300

    fixed vulnerability issue with flexmark

--- src/main/java/ru/org/linux/util/markdown/FlexmarkMarkdownFormatter.java
+++ src/main/java/ru/org/linux/util/markdown/FlexmarkMarkdownFormatter.java
@@ -13,6 +13,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 /*
  *@author bvn13 at 2018-11-11
@@ -21,9 +24,12 @@ import java.util.Arrays;
 @Qualifier("flexmark")
 public class FlexmarkMarkdownFormatter implements MarkdownFormatter {
 
-    MutableDataSet options;
-    Parser parser;
-    HtmlRenderer renderer;
+    private MutableDataSet options;
+    private Parser parser;
+    private HtmlRenderer renderer;
+
+    private static final Pattern PATTERN_ESCAPE_SCRIPT_TAG = Pattern.compile("(<script)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+    private static final Pattern PATTERN_ESCAPE_SCRIPT_CLOSE_TAG = Pattern.compile("(</script)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
 
     public FlexmarkMarkdownFormatter() {
         options = new MutableDataSet();
@@ -34,9 +40,14 @@ public class FlexmarkMarkdownFormatter implements MarkdownFormatter {
                 StrikethroughExtension.create(),
                 YouTubeLinkExtension.create(),
                 WikiLinkExtension.create(),
-                SuperscriptExtension.create()/*,
+                SuperscriptExtension.create()
+                /*,
                 EnumeratedReferenceExtension.create()*/
         ));
+
+        options.set(HtmlRenderer.SUPPRESSED_LINKS, "javascript:.*");
+        //options.set(Parser.HTML_BLOCK_PARSER, false);
+        //options.set(HtmlRenderer.ESCAPE_HTML_BLOCKS, true);
 
         // uncomment to convert soft-breaks to hard breaks
         //options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
@@ -47,6 +58,18 @@ public class FlexmarkMarkdownFormatter implements MarkdownFormatter {
 
     @Override
     public String renderToHtml(String content) {
+        //escape script blocks in cause of vulnerability
+
+        Matcher matcherOpenTag = PATTERN_ESCAPE_SCRIPT_TAG.matcher(content);
+        if (matcherOpenTag.find()) {
+            content = matcherOpenTag.replaceAll("&lt;script");
+        }
+
+        Matcher matcherCloseTag = PATTERN_ESCAPE_SCRIPT_CLOSE_TAG.matcher(content);
+        if (matcherCloseTag.find()) {
+            content = matcherCloseTag.replaceAll("&lt;/script");
+        }
+
         // You can re-use parser and renderer instances
         Node document = parser.parse(content);
         String html = renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
