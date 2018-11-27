81c49d9a5eeb8771be24f4ad5cb6477fe05c8956
Maxim Valyanskiy <max.valjanski@gmail.com>
2018-11-27 08:34:43 +0300

    code cleanup

--- src/main/java/ru/org/linux/user/AddPhotoController.java
+++ src/main/java/ru/org/linux/user/AddPhotoController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2016 Linux.org.ru
+ * Copyright 1998-2018 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -100,13 +100,7 @@ public class AddPhotoController {
               .queryParam("nocache", Integer.toString(random.nextInt())).build().encode();
 
       return new ModelAndView(new RedirectView(profileUri.toString()));
-    } catch (IOException ex){
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      return new ModelAndView("addphoto", "error", ex.getMessage());
-    } catch (BadImageException ex){
-      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      return new ModelAndView("addphoto", "error", ex.getMessage());
-    } catch (UserErrorException ex) {
+    } catch (IOException | BadImageException | UserErrorException ex){
       response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
       return new ModelAndView("addphoto", "error", ex.getMessage());
     } finally {
