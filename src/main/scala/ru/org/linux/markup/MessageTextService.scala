2ee0b958de900899a0d285485a2f2020581dfe18
Maxim Valyanskiy <max.valjanski@gmail.com>
2018-12-25 21:14:31 +0300

    markdown: 4*

--- src/main/scala/ru/org/linux/markup/MessageTextService.scala
+++ src/main/scala/ru/org/linux/markup/MessageTextService.scala
@@ -221,7 +221,7 @@ object MarkupPermissions {
       Set(Lorcode)
     } else if (user.isAdministrator) {
       Set(Lorcode, LorcodeUlb, Markdown, Html)
-    } else if (user.getScore >= 500 || user.isCorrector) {
+    } else if (user.getScore >= 400 || user.isCorrector) {
       Set(Lorcode, LorcodeUlb, Markdown)
     } else {
       Set(Lorcode, LorcodeUlb)
