692d4789f885aff305c6913755b8db5dbb998e3f
Maxim Valyanskiy <max.valjanski@gmail.com>
2019-01-10 20:48:04 +0300

    bugfix: пустое поле "разметка" при предпросмотре

--- src/main/java/ru/org/linux/topic/EditTopicController.java
+++ src/main/java/ru/org/linux/topic/EditTopicController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2018 Linux.org.ru
+ * Copyright 1998-2019 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -488,6 +488,8 @@ public class EditTopicController {
                     imageObject
             )
     );
+
+    params.put("mode", oldText.markup().title());
 
     return new ModelAndView("edit", params);
   }
