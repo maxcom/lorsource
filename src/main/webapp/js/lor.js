maxcom/lorsource

commit 240f08de10b7b7ef24f9d9483534de549096bc2f

Fixing possible race conditioin in replace_state() function
https://www.linux.org.ru/forum/lor-source/14805130
This fixes commit c92274db3da2ee9ccdb2b95d8d81ec4d95daf457

src/main/webapp/js/lor.js
@@ -310,6 +310,13 @@ function replace_state() {
             var pathname = [p[0], p[1], p[2], p[3]].join('/');
             // now replace state
             history.replaceState({}, document.title, pathname + '?cid=' + hash[1]);
+            
+            if(document.querySelector('article.msg:target') === null) {
+                // Target comment lost, this may occur due to race condition in some browsers
+                // trying to workaround, scroll to target comment again
+                // (and this function will be triggered once again)
+                document.location.hash = '#comment-' + hash[1];
+            }
         }
     }
 }

commit bba26818845ec360c2302396a739c34511d35d23

lor.js: prevent looping in replace_state(). https://www.linux.org.ru/forum/general/14841331 Issue introduced in 240f08de10b7b7ef24f9d9483534de549096bc2f

src/main/webapp/js/lor.js
@@ -302,6 +302,10 @@ function replace_state() {
     
     if (document.location.hash.indexOf('#comment-') == 0) {
         // Yes, we are viewing a comment
+        
+        // bypass if no such target to avoid looping
+        if (document.querySelector(document.location.hash) === null) return;
+        
         var hash = document.location.hash.split('-');
         if (parseInt(hash[1]) > 0) {
             // OK, comment ID is valid