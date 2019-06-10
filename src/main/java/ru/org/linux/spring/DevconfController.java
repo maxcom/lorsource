69eb2bda7ec52c35c436144d686e3ac93947ca8b
Maxim Valyanskiy <max.valjanski@gmail.com>
2019-06-10 23:32:05 +0300

    devconf 2019

--- src/main/java/ru/org/linux/spring/DevconfController.java
+++ src/main/java/ru/org/linux/spring/DevconfController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2016 Linux.org.ru
+ * Copyright 1998-2019 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -51,7 +51,7 @@ public class DevconfController {
 
     User user = tmpl.getCurrentUser();
 
-    if (!"devconf2017".equals(msg)) {
+    if (!"devconf2019".equals(msg)) {
       throw new UserErrorException("Неправильный код, прочитайте текст новости");
     }
 
