5b4bd579e03a253ec408aac826a833e2f022a533
Maxim Valyanskiy <max.valjanski@gmail.com>
2021-03-10 22:42:52 +0300

    fix chat name

--- src/main/scala/ru/org/linux/telegram/TelegramBotActor.scala
+++ src/main/scala/ru/org/linux/telegram/TelegramBotActor.scala
@@ -47,7 +47,7 @@ class TelegramBotActor(dao: TelegramPostsDao, wsClient: StandaloneWSClient, conf
           } else {
             wsClient
               .url(s"https://api.telegram.org/bot${config.getTelegramToken}/sendMessage")
-              .addQueryStringParameters("chat_id" -> "best_of_lor")
+              .addQueryStringParameters("chat_id" -> "@best_of_lor")
               .addQueryStringParameters("text" -> URLEncoder.encode(topic.getLink, StandardCharsets.UTF_8))
               .get()
               .pipeTo(self)
