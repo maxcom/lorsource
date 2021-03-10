07c16bc9d55ed9d983b404be3795fbc184b41ab0
Maxim Valyanskiy <max.valjanski@gmail.com>
2021-03-10 22:36:31 +0300

    fix sql

--- src/main/scala/ru/org/linux/telegram/TelegramPostsDao.scala
+++ src/main/scala/ru/org/linux/telegram/TelegramPostsDao.scala
@@ -37,11 +37,22 @@ class TelegramPostsDao(ds:DataSource) {
   def hotTopic: Option[Topic] = {
     jdbcTemplate.queryAndMap(
       """
-        |select topic, count (distinct comments.userid)
-        |  from comments join users on comments.userid=users.id join topics on (comments.topic=topics.id)
-        |where comments.postdate>CURRENT_TIMESTAMP-'6 hour'::interval and score>=100
-        |  and topics.id not in (select topic_id from telegram_posts) and not topics.deleted AND not comments.deleted
-        |group by topic having count (distinct comments.userid)>=15 order by count desc limit 1
+        |select topics.postdate, topics.id as msgid, topics.userid, topics.title,
+        |  topics.groupid as guid, topics.url, topics.linktext, topics.ua_id,
+        |  urlname, section, topics.sticky, topics.postip,
+        |  COALESCE(commitdate, topics.postdate)<(CURRENT_TIMESTAMP-sections.expire) as expired, topics.deleted, lastmod, commitby,
+        |  commitdate, topics.stat1, postscore, topics.moderate, notop,
+        |  topics.resolved, minor, draft, allow_anonymous
+        |from topics join groups ON (groups.id=topics.groupid) join sections on (sections.id=groups.section)
+        |where topics.id in (
+        |  select topic from comments join users on comments.userid=users.id join topics on (comments.topic=topics.id)
+        |    where comments.postdate>CURRENT_TIMESTAMP-'6 hour'::interval and score>=100
+        |      and topics.id not in (select topic_id from telegram_posts) and not topics.deleted AND not comments.deleted
+        |      and not notop and not draft
+        |    group by topic
+        |    having count (distinct comments.userid)>=15
+        |    order by count(distinct comments.userid) desc
+        |    limit 1)
         |""".stripMargin) { (resultSet, _) => new Topic(resultSet) }.headOption
   }
 }
