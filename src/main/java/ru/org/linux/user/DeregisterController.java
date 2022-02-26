22464ebadac86559a4152082826a1f41194dc0f4
Maxim Valyanskiy <max.valjanski@gmail.com>
2022-02-26 18:09:53 +0300

    frozen пользователи не могут себя заблокировать

--- src/main/java/ru/org/linux/user/DeregisterController.java
+++ src/main/java/ru/org/linux/user/DeregisterController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2018 Linux.org.ru
+ * Copyright 1998-2022 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -90,6 +90,7 @@ public class DeregisterController {
 
     User user = tmpl.getCurrentUser();
     user.checkAnonymous();
+    user.checkFrozen(errors);
 
     if (user.getScore() < 100) {
       throw new AccessViolationException("Удаление аккаунта недоступно для пользователей со score < 100");
