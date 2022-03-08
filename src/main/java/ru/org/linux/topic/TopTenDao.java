f75c7b846eb325991e5599829f4fef380a63c75a
Maxim Valyanskiy <max.valjanski@gmail.com>
2022-03-08 11:36:26 +0300

    top10: убрал топики с выключенными комментариями
    + вернул туда talks и linux-org-ru

--- src/main/java/ru/org/linux/topic/TopTenDao.java
+++ src/main/java/ru/org/linux/topic/TopTenDao.java
@@ -42,7 +42,8 @@ public class TopTenDao {
         "from topics " +
         "join groups on groups.id = topics.groupid" +
       " where topics.postdate>(CURRENT_TIMESTAMP-'1 month 1 day'::interval) and not deleted and not notop " +
-      " and groupid!=8404 and groupid!=4068 and groupid!=19390 order by c desc, msgid limit 10";
+      " and topics.postscore is distinct from " + TopicPermissionService.POSTSCORE_HIDE_COMMENTS + " " +
+      " and groupid!=19390 order by c desc, msgid limit 10";
 
     return jdbcTemplate.query(sql, (rs, i) -> new TopTenMessageDTO(
             sectionService.getSection(rs.getInt("section")).getSectionLink()+rs.getString("urlname")+ '/' +rs.getInt("msgid"),
