e8b80af068dcbc6d0a37923f198b8b610f967d7d
Maxim Valyanskiy <max.valjanski@gmail.com>
2022-03-31 12:30:02 +0300

    фикс потенциальной дыры в Spring MVC
    https://www.praetorian.com/blog/spring-core-jdk9-rce/

---
+++ src/main/java/ru/org/linux/util/BinderControllerAdvice.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 1998-2022 Linux.org.ru
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package ru.org.linux.util;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+@Order(10000)
+public class BinderControllerAdvice {
+  @InitBinder
+  public void setAllowedFields(WebDataBinder dataBinder) {
+
+    String[] denylist = new String[]{"class.", "Class.", ".class.", ".Class."};
+
+    dataBinder.setDisallowedFields(denylist);
+  }
+}
