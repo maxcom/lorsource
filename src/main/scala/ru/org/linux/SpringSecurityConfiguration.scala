9ab1013258bf36628c29b7678624ebac8841a4c3
Maxim Valyanskiy <max.valjanski@gmail.com>
2022-06-05 00:28:00 +0300

    tune spring security

--- src/main/scala/ru/org/linux/SpringSecurityConfiguration.scala
+++ src/main/scala/ru/org/linux/SpringSecurityConfiguration.scala
@@ -3,22 +3,13 @@ package ru.org.linux
 import org.springframework.context.annotation.{Bean, Configuration}
 import org.springframework.security.web.firewall.StrictHttpFirewall
 
-import java.nio.charset.StandardCharsets
-import java.util.regex.Pattern
-
 @Configuration
 class SpringSecurityConfiguration {
-  private val allowed: Pattern = Pattern.compile("[\\p{IsAssigned}&&[^\\p{IsControl}]]*")
-
   @Bean
   def httpFirewall: StrictHttpFirewall = {
     val firewall = new StrictHttpFirewall
 
-    firewall.setAllowedHeaderValues { header =>
-      val parsed = new String(header.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8)
-
-      allowed.matcher(parsed).matches
-    }
+    firewall.setAllowedHeaderValues(_ => true )
 
     firewall
   }
