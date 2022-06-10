dc83f3b36d57d1fa4fe4abbe3a477fe32bd316bc
Maxim Valyanskiy <max.valjanski@gmail.com>
2022-06-10 19:41:06 +0300

    при модификации тега переиндексим комменты
    без этого статистика странная получается

--- src/main/scala/ru/org/linux/tag/TagModificationService.scala
+++ src/main/scala/ru/org/linux/tag/TagModificationService.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2016 Linux.org.ru
+ * Copyright 1998-2022 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -39,7 +39,7 @@ class TagModificationService(val transactionManager: PlatformTransactionManager,
 
     tagDao.changeTag(oldTagId, tagName)
 
-    topicTagDao.processTopicsByTag(oldTagId, searchQueueSender.updateMessageOnly)
+    topicTagDao.processTopicsByTag(oldTagId, searchQueueSender.updateMessage(_, true))
   }
 
   /**
@@ -62,7 +62,7 @@ class TagModificationService(val transactionManager: PlatformTransactionManager,
       toUpdate
     }
 
-    toUpdate foreach searchQueueSender.updateMessageOnly
+    toUpdate.foreach(searchQueueSender.updateMessage(_, true))
   }
 
   /**
@@ -98,6 +98,6 @@ class TagModificationService(val transactionManager: PlatformTransactionManager,
       newTagId
     }
 
-    topicTagDao.processTopicsByTag(newTagId, searchQueueSender.updateMessageOnly)
+    topicTagDao.processTopicsByTag(newTagId, searchQueueSender.updateMessage(_, true))
   }
 }
