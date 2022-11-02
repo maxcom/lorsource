f620b975127324ce465daaaf354ba5881bc88566
Maxim Valyanskiy <max.valjanski@gmail.com>
2022-11-02 22:11:26 +0300

    dateinterval logic -> DateFormats
    + немного улучшил логику

--- src/main/scala/ru/org/linux/site/DateFormats.scala
+++ src/main/scala/ru/org/linux/site/DateFormats.scala
@@ -14,11 +14,12 @@
  */
 package ru.org.linux.site
 
-import org.joda.time.DateTimeZone
+import org.joda.time.{DateTime, DateTimeZone}
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.format.DateTimeFormatter
 import org.joda.time.format.ISODateTimeFormat
-import java.util.Locale
+
+import java.util.{Date, Locale}
 
 object DateFormats {
   private val RussianLocale = new Locale("ru")
@@ -32,9 +33,55 @@ object DateFormats {
 
   def getDefault(tz: DateTimeZone): DateTimeFormatter = Default.withZone(tz)
 
-  def getShort(tz: DateTimeZone): DateTimeFormatter = Short.withZone(tz)
+  private def short(tz: DateTimeZone): DateTimeFormatter = Short.withZone(tz)
+  private def time(tz: DateTimeZone): DateTimeFormatter = Time.withZone(tz)
+  private def date(tz: DateTimeZone): DateTimeFormatter = Date.withZone(tz)
 
-  def time(tz: DateTimeZone): DateTimeFormatter = Time.withZone(tz)
+  def formatInterval(date: Date, timezone: DateTimeZone): String = {
+    val diff = System.currentTimeMillis - date.getTime
+    val c = new DateTime(date.getTime)
 
-  def date(tz: DateTimeZone): DateTimeFormatter = Date.withZone(tz)
+    val today = DateTime.now.withZone(timezone).withTimeAtStartOfDay
+    val yesterday = DateTime.now.withZone(timezone).minusDays(1).withTimeAtStartOfDay
+
+    if (diff < 2 * 1000 * 60) {
+      "минуту назад"
+    } else if (diff < 1000 * 60 * 60) {
+      val min = diff / (1000 * 60)
+
+      if (min % 10 < 5 && min % 10 > 1 && (min > 20 || min < 10)) {
+        min + "&nbsp;минуты назад"
+      } else if (min % 10 == 1 && min > 20) {
+        min + "&nbsp;минута назад"
+      } else {
+        min + "&nbsp;минут назад"
+      }
+    } else if (c.isAfter(today)) {
+      "сегодня " + time(timezone).print(c)
+    } else if (c.isAfter(yesterday)) {
+      "вчера " + time(timezone).print(c)
+    } else {
+      short(timezone).print(c)
+    }
+  }
+
+  def formatCompactInterval(date: Date, timezone: DateTimeZone): String = {
+    val diff = System.currentTimeMillis - date.getTime
+    val c = new DateTime(date.getTime)
+
+    val today = DateTime.now.withZone(timezone).withTimeAtStartOfDay
+    val yesterday = DateTime.now.withZone(timezone).minusDays(1).withTimeAtStartOfDay
+
+    if (diff < 1000 * 60 * 60) {
+      val min = Math.max(1, diff / (1000 * 60))
+
+      min + "&nbsp;мин"
+    } else if (diff < 1000 * 60 * 60 * 12 || c.isAfter(today)) {
+      time(timezone).print(c)
+    } else if (c.isAfter(yesterday)) {
+      "вчера"
+    } else {
+      DateFormats.date(timezone).print(c)
+    }
+  }
 }
--- src/main/webapp/WEB-INF/tags/dateinterval.tag
+++ src/main/webapp/WEB-INF/tags/dateinterval.tag
@@ -18,51 +18,11 @@
   --%>
 <%@ attribute name="compact" required="false" type="java.lang.Boolean"%>
 <%@ attribute name="date" required="true" type="java.util.Date" %><time datetime="<%= DateFormats.Iso8601().print(date.getTime()) %>"><%
-  long diff = System.currentTimeMillis() - date.getTime();
   boolean comp = compact!=null && compact;
-  DateTime c = new DateTime(date.getTime());
-  DateTimeZone timezone = (DateTimeZone) request.getAttribute("timezone");
 
-  DateTime today = DateTime.now().withZone(timezone).withTimeAtStartOfDay();
-  DateTime yesterday = DateTime.now().withZone(timezone).minusDays(1).withTimeAtStartOfDay();
-
-  if (diff<2*1000*60) {
-    if (comp) {
-      out.print("1 мин");
-    } else {
-      out.print("минуту назад");
-    }
-  } else if (diff<1000*60*60) {
-    long min = diff / (1000 * 60);
-
-    if (comp) {
-      out.print(min+"&nbsp;мин");
-    } else {
-      if (min % 10 < 5 && min % 10 > 1 && (min > 20 || min < 10)) {
-        out.print(min + "&nbsp;минуты назад");
-      } else if (min % 10 == 1 && min > 20) {
-        out.print(min + "&nbsp;минута назад");
-      } else {
-        out.print(min + "&nbsp;минут назад");
-      }
-    }
-  } else if (c.isAfter(today)) {
-    if (comp) {
-      out.print(DateFormats.time(timezone).print(c));
-    } else {
-      out.print("сегодня " + DateFormats.time(timezone).print(c));
-    }
-  } else if (c.isAfter(yesterday)) {
-    if (comp) {
-      out.print("вчера");
-    } else {
-      out.print("вчера " + DateFormats.time(timezone).print(c));
-    }
+  if (comp) {
+    out.print(DateFormats.formatCompactInterval(date, (DateTimeZone) request.getAttribute("timezone")));
   } else {
-    if (comp) {
-      out.print(DateFormats.date(timezone).print(c));
-    } else {
-      out.print(DateFormats.getShort(timezone).print(c));
-    }
+    out.print(DateFormats.formatInterval(date, (DateTimeZone) request.getAttribute("timezone")));
   }
 %></time>
