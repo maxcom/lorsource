a8c10a8640011b20caa199f35440684c3aa8a367
Maxim Valyanskiy <max.valjanski@gmail.com>
2022-11-06 21:09:41 +0300

    timezone для зарегистрированных

--- src/main/scala/ru/org/linux/auth/CommonContextFilter.scala
+++ src/main/scala/ru/org/linux/auth/CommonContextFilter.scala
@@ -35,7 +35,7 @@ class CommonContextFilter extends GenericFilterBean with InitializingBean {
     val response = res.asInstanceOf[HttpServletResponse]
     val currentUser = AuthUtil.getCurrentUser
 
-    if (currentUser!=null && currentUser.getScore >= 100) {
+    if (currentUser!=null) {
       val cookies = getCookies(request)
       val timezoneName = cookies.get("tz")
 
