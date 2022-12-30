fca3ee1a47ae17513ba69e61e21d1fe4b066b172
Maxim Valyanskiy <max.valjanski@gmail.com>
2022-12-30 17:57:22 +0300

    code cleanup

--- src/main/java/ru/org/linux/comment/ReplyInfo.java
+++ src/main/java/ru/org/linux/comment/ReplyInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2016 Linux.org.ru
+ * Copyright 1998-2022 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -15,15 +15,10 @@
 
 package ru.org.linux.comment;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import ru.org.linux.site.PublicApi;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Date;
 
-@PublicApi
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReplyInfo {
   private final int id;
   private final String author;
