e72d63951186d4fff768d4a22428fb06e4683f27
Maxim Valyanskiy <max.valjanski@gmail.com>
2022-12-30 18:30:06 +0300

    MemoriesController -> Scala

---
+++ src/main/scala/ru/org/linux/user/MemoriesController.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 1998-2022 Linux.org.ru
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package ru.org.linux.user
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.{RequestMapping, RequestMethod, RequestParam, ResponseBody}
+import ru.org.linux.auth.AccessViolationException
+import ru.org.linux.auth.AuthUtil.AuthorizedOnly
+import ru.org.linux.topic.TopicDao
+
+import java.util
+import scala.jdk.CollectionConverters.MapHasAsJava
+
+@Controller
+class MemoriesController(messageDao: TopicDao, memoriesDao: MemoriesDao) {
+  @ResponseBody
+  @RequestMapping(value = Array("/memories.jsp"), params = Array("add"), method = Array(RequestMethod.POST))
+  def add(@RequestParam("msgid") msgid: Int,
+          @RequestParam("watch") watch: Boolean): util.Map[String, Integer] = AuthorizedOnly { currentUser =>
+    val topic = messageDao.getById(msgid)
+
+    if (topic.deleted) {
+      throw new UserErrorException("Тема удалена")
+    }
+
+    val id = memoriesDao.addToMemories(currentUser.user, topic, watch)
+    val memoriesInfo = memoriesDao.getTopicInfo(msgid, currentUser.user)
+
+    val count = if (watch) {
+      memoriesInfo.watchCount
+    } else {
+      memoriesInfo.favsCount
+    }
+
+    Map("id" -> Integer.valueOf(id), "count" -> Integer.valueOf(count)).asJava
+  }
+
+  @ResponseBody
+  @RequestMapping(value = Array("/memories.jsp"), params = Array("remove"), method = Array(RequestMethod.POST))
+  def remove(@RequestParam("id") id: Int): Int = AuthorizedOnly { currentUser =>
+    memoriesDao.getMemoriesListItem(id).map { m =>
+      if (m.getUserid != currentUser.user.getId) {
+        throw new AccessViolationException("Нельзя удалить чужую запись")
+      }
+
+      memoriesDao.delete(id)
+
+      val memoriesInfo = memoriesDao.getTopicInfo(m.getTopic, currentUser.user)
+
+      if (m.isWatch) {
+        memoriesInfo.watchCount
+      } else {
+        memoriesInfo.favsCount
+      }
+    }.orElse(-1)
+  }
+}
