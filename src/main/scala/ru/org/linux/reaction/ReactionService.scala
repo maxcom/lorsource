6a48860da0371fc8fef47673d066920e636b0e9c
Dmitry Atamanov <dataman@tutanota.com>
2023-09-21 17:34:32 +0500

    Реакция "грусть-печаль"

--- src/main/scala/ru/org/linux/reaction/ReactionService.scala
+++ src/main/scala/ru/org/linux/reaction/ReactionService.scala
@@ -74,7 +74,8 @@ object ReactionService {
     "\uD83E\uDD14" -> "задумчивое лицо",
     "\uD83E\uDD21" -> "лицо клоуна",
     "\u2615\u2615" -> "два чая этому господину!",
-    "\uD83E\uDE97" -> "боян!!!1111")
+    "\uD83E\uDE97" -> "боян!!!1111",
+    "\uD83D\uDE22" -> "грусть-печаль")
 
   val AllowedReactions: Set[String] = DefinedReactions.keySet
 }
