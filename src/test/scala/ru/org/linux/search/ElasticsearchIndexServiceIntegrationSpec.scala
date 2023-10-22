f686812490918f83719a174329495c889cb00d4c
Maxim Valyanskiy <max.valjanski@gmail.com>
2023-10-22 12:47:46 +0300

    opensearch 2.11

--- src/test/scala/ru/org/linux/search/ElasticsearchIndexServiceIntegrationSpec.scala
+++ src/test/scala/ru/org/linux/search/ElasticsearchIndexServiceIntegrationSpec.scala
@@ -65,7 +65,7 @@ class ElasticsearchIndexServiceIntegrationSpec extends SpecificationWithJUnit {
 class SearchIntegrationTestConfiguration {
   @Bean(destroyMethod="close")
   def elasticClient: ElasticClient = {
-    val container = new OpensearchContainer(DockerImageName.parse("opensearchproject/opensearch:2.10.0"))
+    val container = new OpensearchContainer(DockerImageName.parse("opensearchproject/opensearch:2.11.0"))
     container.start()
 
     val host = container.getHttpHostAddress
