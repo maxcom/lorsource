2eea36ace0e074aa4551a107def2af863b870588
Maxim Valyanskiy <max.valjanski@gmail.com>
2024-09-28 11:37:50 +0300

    backport fixes from spring-6 branch

--- src/main/java/ru/org/linux/search/SearchQueueSender.java
+++ src/main/java/ru/org/linux/search/SearchQueueSender.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2022 Linux.org.ru
+ * Copyright 1998-2024 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -19,7 +19,6 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
