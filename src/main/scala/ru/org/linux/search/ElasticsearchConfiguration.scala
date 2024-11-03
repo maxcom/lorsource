ac0ae563394e9b9a201051c6cfe1a306531d3a63
Maxim Valyanskiy <max.valjanski@gmail.com>
2024-11-03 14:46:04 +0300

    поднял socket timeout на поиске

--- src/main/scala/ru/org/linux/search/ElasticsearchConfiguration.scala
+++ src/main/scala/ru/org/linux/search/ElasticsearchConfiguration.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2022 Linux.org.ru
+ * Copyright 1998-2024 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -17,6 +17,8 @@ package ru.org.linux.search
 
 import com.sksamuel.elastic4s.http.JavaClient
 import com.sksamuel.elastic4s.{ElasticClient, ElasticProperties}
+import org.apache.http.client.config.RequestConfig
+import org.elasticsearch.client.RestClientBuilder.RequestConfigCallback
 import org.springframework.context.annotation.{Bean, Configuration}
 import ru.org.linux.spring.SiteConfig
 
@@ -24,6 +26,10 @@ import ru.org.linux.spring.SiteConfig
 class ElasticsearchConfiguration(config: SiteConfig) {
   @Bean(destroyMethod = "close")
   def client: ElasticClient = {
-    ElasticClient(JavaClient(ElasticProperties(config.getElasticsearch)))
+    ElasticClient(JavaClient(ElasticProperties(config.getElasticsearch), new RequestConfigCallback() {
+      override def customizeRequestConfig(requestConfigBuilder: RequestConfig.Builder): RequestConfig.Builder = {
+        requestConfigBuilder.setSocketTimeout(120000)
+      }
+    }))
   }
 }
