63441737d16d630ff9c0ee6a5478cf91d1d0bc9e
Maxim Valyanskiy <max.valjanski@gmail.com>
2025-02-01 11:30:50 +0300

    доп картинки для 2*

--- src/main/java/ru/org/linux/group/GroupPermissionService.scala
+++ src/main/java/ru/org/linux/group/GroupPermissionService.scala
@@ -123,7 +123,7 @@ class GroupPermissionService(sectionService: SectionService, deleteInfoDao: Dele
   def additionalImageLimit(section: Section)(implicit currentUser: AnySession): Int = {
     if (isImagePostingAllowed(section) &&
         section.getId == Section.SECTION_GALLERY &&
-        currentUser.userOpt.exists(_.getScore >= 300)) {
+        currentUser.userOpt.exists(_.getScore >= 200)) {
       3
     } else {
       0
