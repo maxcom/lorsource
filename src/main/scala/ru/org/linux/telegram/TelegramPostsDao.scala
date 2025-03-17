723223929c092d46214360fbd171911fc008d278
Maxim Valyanskiy <max.valjanski@gmail.com>
2025-03-17 21:04:45 +0300

    поднял порог постинга в  telegram

--- src/main/scala/ru/org/linux/telegram/TelegramPostsDao.scala
+++ src/main/scala/ru/org/linux/telegram/TelegramPostsDao.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 1998-2022 Linux.org.ru
+ * Copyright 1998-2025 Linux.org.ru
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
@@ -71,5 +71,5 @@ class TelegramPostsDao(ds: DataSource) {
 }
 
 object TelegramPostsDao {
-  val RequiredActiveUsers = 14
+  val RequiredActiveUsers = 15
 }
